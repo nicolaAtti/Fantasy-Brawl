1eaa2edb5981f770504f362c4c7618a89ae4f53c
Nicola Atti <nicola.atti@studio.unibo.it>
2018-11-15 00:12:36 +0100

    Reduction of code repetition according to design patterns and testing

---
+++ client/src/main/scala/model/StatModifiers.scala
@@ -0,0 +1,10 @@
+package model
+
+class StatModifiers(strMod: Double, agiMod: Double, spiMod: Double, intMod: Double, resMod: Double, hpMod: Double) {
+  def getStrMod: Double = strMod
+  def getAgiMod: Double = agiMod
+  def getSpiMod: Double = spiMod
+  def getIntMod: Double = intMod
+  def getResMod: Double = resMod
+  def getHpMod: Double = hpMod
+}
---
+++ client/src/main/scala/model/Statistics.scala
@@ -0,0 +1,9 @@
+package model
+
+class Statistics(strength: Int,agility: Int,spirit: Int,intelligence: Int,resistance: Int){
+  def getStrength: Int = strength
+  def getAgility: Int = agility
+  def getSpirit: Int = spirit
+  def getIntelligence: Int = intelligence
+  def getResistance: Int = resistance
+}
