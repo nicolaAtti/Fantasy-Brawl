2441020b3c8da9a7e403aa62aa13324b9a92db96
Nicola <nicola.atti@studio.unibo.it>
2018-11-15 17:19:25 +0100

    Worked on factory for StatModifiers, and refactoring

--- client/src/main/scala/model/Character.scala
+++ client/src/main/scala/model/Character.scala
@@ -1,9 +1,6 @@
-package main.scala.model
+package model
 
-//import main.resources.Utility
-import model.{Statistics, StatModifiers}
-
-import scala.math._
+import main.resources.Utility
 
 //Aspetti condivisi : le 5 statistiche (che vanno nel costruttore) i metodi per accedervici ,
 //Aspetti differenti: i modificatori con cui vengono aleterati i valori delle statistiche secondarie
@@ -13,6 +10,7 @@ import scala.math._
   *
   * @param charName   the name of the Character
   * @param stats      the statistics of the Character
+  * @param statMods   the class statistic modifier
   * @author Nicola Atti
   */
 trait Character{
@@ -22,41 +20,43 @@ trait Character{
   var statMods: StatModifiers
 
 
-  def getPhysDamage: Int = floor(stats.getStrength * statMods.getStrMod)toInt
-  def getPysCritDamage: Int = floor((stats.getStrength * statMods.getStrMod)/5 +150)toInt
-  def getSpeed: Int = floor((stats.getAgility * statMods.getAgiMod)/10)toInt
-  def getCritChance: Int = floor((stats.getAgility * statMods.getAgiMod)/2)toInt
-  def getMagDefence: Int = floor(stats.getSpirit * statMods.getSpiMod)toInt
-  def getMaxMP: Int = floor((stats.getSpirit * statMods.getSpiMod)*5)toInt
-  def getMagDamage: Int = floor(stats.getIntelligence * statMods.getIntMod)toInt
-  def getMagicCritDamage: Int = floor((stats.getIntelligence * statMods.getIntMod)/10 +150)toInt
-  def getMaxHP: Int = floor((stats.getResistance * statMods.getHpMod)*10)toInt
-  def getPhysDefence: Int = floor(stats.getResistance * statMods.getResMod)toInt
+  def getPhysDamage: Int = Utility.roundDown(stats.strength * statMods.strMod)
+  def getPysCritDamage: Int = Utility.roundDown((stats.strength * statMods.strMod)/5 +150)
+  def getSpeed: Int = Utility.roundDown((stats.agility * statMods.agiMod)/10)
+  def getCritChance: Int = Utility.roundDown((stats.agility * statMods.agiMod)/2)
+  def getMagDefence: Int = Utility.roundDown(stats.spirit * statMods.spiMod)
+  def getMaxMP: Int = Utility.roundDown((stats.spirit * statMods.spiMod)*5)
+  def getMagDamage: Int = Utility.roundDown(stats.intelligence * statMods.intMod)
+  def getMagicCritDamage: Int = Utility.roundDown((stats.intelligence * statMods.intMod)/10 +150)
+  def getMaxHP: Int = Utility.roundDown((stats.resistance * statMods.hpMod)*10)
+  def getPhysDefence: Int = Utility.roundDown(stats.resistance * statMods.resMod)
+
+  var currHP: Int = getMaxHP
+  var currMP: Int = getMaxMP
 
   //Passagli la funzione della mossa, sia essa speciale, attacco base o passare il turno   STRATEGY
   def doMove = ???
 
-  /*def changeCurrHPMP(stat: String,function: (Int,Int)=>Int , value: Int): Unit = stat match {
-    case "HP" => if (function(currHP, value) > maxHP) {
-      currHP = maxHP
+  def changeCurrHPMP(stat: String,function: (Int,Int)=>Int , value: Int): Unit = stat.toUpperCase match {
+    case "HP" => if (function(currHP, value) > getMaxHP) {
+      currHP = getMaxHP
     } else if (function(currHP, value) < 0) {
       currHP = 0
     } else {
       currHP = function(currHP, value)
     }
-    case "MP" => if (function(currMP, value) > maxMP) {
-      currMP = maxMP
+    case "MP" => if (function(currMP, value) > getMaxMP) {
+      currMP = getMaxMP
     } else if (function(currMP, value) < 0) {
       currMP = 0
     } else {
       currMP = function(currMP, value)
     }
-  }*/
+  }
 }
 
 private case class Warrior(override var charName: String, override var stats: Statistics, override var statMods: StatModifiers) extends Character {
 }
-
 private case class Thief(override var charName: String, override var stats: Statistics, override var statMods: StatModifiers) extends Character {
 }
 private case class Wizard(override var charName: String, override var stats: Statistics, override var statMods: StatModifiers) extends Character {
@@ -66,11 +66,13 @@ private case class Healer(override var charName: String,override var stats: Stat
 
 //Companion object
 object CharacterFactory{
-        def apply(role: String,charName: String,stats: Statistics,mods: StatModifiers): Character = role match{
-                case "Warrior" => new Warrior(charName,stats,mods)
-                case "Thief" => new Thief(charName,stats,mods)
-                case "Wizard" => new Wizard(charName,stats,mods)
-                case "Healer" => new Healer(charName,stats,mods)
+        def apply(role: String,charName: String,stats: Statistics): Character = role match{
+                case "Warrior" => Warrior(charName,stats,StatModsFactory(role))
+                case "Thief" => Thief(charName,stats,StatModsFactory(role))
+                case "Wizard" => Wizard(charName,stats,StatModsFactory(role))
+                case "Healer" => Healer(charName,stats,StatModsFactory(role))
         }
 
 }
+
+
