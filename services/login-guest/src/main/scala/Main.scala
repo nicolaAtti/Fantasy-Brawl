3382aa60eadd3149d2b52284ff17cf38ec03f01b
Marco Canducci <marco.candoz@gmail.com>
2018-11-23 00:58:06 +0100

    Implemented first incomplete version of the login service (as guest)
    Still to do:
      - Add MongDB support
      - Put messages and queues in a shared package

---
+++ services/login-guest/src/main/scala/Main.scala
@@ -0,0 +1,61 @@
+import akka.actor.{ActorSystem, Props}
+import com.spingo.op_rabbit.properties.ReplyTo
+import com.spingo.op_rabbit._
+
+import scala.concurrent.ExecutionContext
+import play.api.libs.json._
+import PlayJsonSupport._
+
+/** Entry point of the service that handles the login for guests users.
+  *
+  * It reads and update (incrementing by one) a counter that acts as unique guest identifier.
+  */
+object Main extends App {
+
+  final val Log = true
+  final val LogMessage = "Received a new login request"
+  final val DetailsPrefix = "Details: "
+
+  val rabbitControl = ActorSystem().actorOf(Props[RabbitControl])
+  implicit val recoveryStrategy = RecoveryStrategy.nack(false)
+
+  import messages._
+
+  implicit val requestFormat = Json.format[LoginGuestRequest]
+  implicit val responseFormat = Json.format[LoginGuestResponse]
+
+  import Queues._
+  val requestQueue = Queue(LoginGuestRequestQueue, durable = false, autoDelete = true)
+
+  import ExecutionContext.Implicits.global
+
+  val subscription = Subscription.run(rabbitControl) {
+    import Directives._
+    channel(qos = 3) {
+      consume(requestQueue) {
+        (body(as[LoginGuestRequest]) & optionalProperty(ReplyTo)) {
+
+          (request, replyTo) => {
+            if (Log) {
+              println(LogMessage)
+              request.details match {
+                case Some(d) => println(DetailsPrefix + d)
+              }
+            }
+
+            val response = LoginGuestResponse(guestId = Some(getAndUpdateFromDb), details = None)
+            rabbitControl ! Message.queue(response, queue = replyTo.get)
+            ack
+          }
+
+        }
+      }
+    }
+  }
+
+  // Temporary mockup function
+  def getAndUpdateFromDb: Int = {
+    scala.util.Random.nextInt()
+  }
+
+}
---
+++ services/login-guest/src/main/scala/messages/LoginGuestResponse.scala
@@ -0,0 +1,8 @@
+package messages
+
+/** The response message that the client will receive after a LoginGuestRequest.
+  *
+  * @param guestId the incremental ID assigned to the guest
+  * @param details eventual details about the response that the server might attach for the client
+  */
+case class LoginGuestResponse(guestId: Option[Int], details: Option[String])
