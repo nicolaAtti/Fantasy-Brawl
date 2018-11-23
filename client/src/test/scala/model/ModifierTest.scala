f097c0fa041c6691a5d5d0a79c4e91d1b0a94337
Nicola Atti <nicola.atti@studio.unibo.it>
2018-11-23 20:54:45 +0100

    Created Status object, fixed affliction and modifier removal bug

--- client/src/test/scala/model/ModifierTest.scala
+++ client/src/test/scala/model/ModifierTest.scala
@@ -20,30 +20,30 @@ class ModifierTest extends FunSuite{
   }
 
   test("Test the correct application of a new modifier"){
-    assert(warrior.getPhysDamage == 102)
-    warrior.addModifier(newModifier)
-    assert(warrior.getPhysDamage == 122)
+    assert(warrior.calculatePhysDamage == 102)
+    warrior.status.addModifier(newModifier)
+    assert(warrior.calculatePhysDamage == 122)
   }
 
   test("Test the application of multiple modifiers"){
-    warrior.addModifier(anotherModifier)
-    assert(warrior.getPhysDamage == 162)
+    warrior.status.addModifier(anotherModifier)
+    assert(warrior.calculatePhysDamage == 162)
   }
   test("After a turn the modifiers timer should decrease by 1"){
-    warrior.newTurnCountdown()
+    warrior.status.newTurnCountdown()
     assert(newModifier.turnDuration == 2)
     assert(anotherModifier.turnDuration == 1)
   }
   test("The adding of an existing modifier should 'reset' the timer of the previous one") {
     val newNewModifier = Modifier("battle_prowess","PHYS_DAMAGE",3,20)
-    warrior.addModifier(newNewModifier)
-    assert(warrior.statMods.filter(mod => mod.modId equals newModifier.modId).head.turnDuration == 3)
-    assert(warrior.getPhysDamage == 162)
+    warrior.status.addModifier(newNewModifier)
+    assert(warrior.status.modifiers.filter(mod => mod.modId equals newModifier.modId).head.turnDuration == 3)
+    assert(warrior.calculatePhysDamage == 162)
   }
 
   test("A modifiers that reaches 0 in turnDuration should be removed from the list"){
-    warrior.newTurnCountdown()
+    warrior.status.newTurnCountdown()
     assert(anotherModifier.turnDuration == 0)
-    assert(warrior.getPhysDamage == 122)
+    assert(warrior.calculatePhysDamage == 122)
   }
 }
