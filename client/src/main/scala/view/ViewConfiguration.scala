53facc16b79424ad1f50afcadce82215564f2699
Schiavi91 <38749012+Schiavi91@users.noreply.github.com>
2018-11-24 23:37:05 +0100

    Create a client fat jar and deploy with travis
    * Test travis jar deploy

--- client/src/main/scala/view/ViewConfiguration.scala
+++ client/src/main/scala/view/ViewConfiguration.scala
@@ -2,9 +2,9 @@ package view
 
 object ViewConfiguration {
   val LoginTitle: String = "Fantasy Brawl - Login"
-  val LoginForm: String = "../LoginForm.fxml"
+  val LoginForm: String = "LoginForm.fxml"
   val TeamSelectionTitle: String = "Fantasy Brawl - Team Selection"
-  val TeamSelectionForm: String = "../TeamSelectionForm.fxml"
+  val TeamSelectionForm: String = "TeamSelectionForm.fxml"
   val WaitingTitle: String = "Fantasy Brawl - Login"
-  val WaitingForm: String = "../WaitingForm.fxml"
+  val WaitingForm: String = "WaitingForm.fxml"
 }
