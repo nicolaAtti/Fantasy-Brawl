8f0f281e110b008fb1381909ca7ed3b9e68aa983
Daniele Schiavi <daniele.schiavi@studio.unibo.it>
2018-11-25 16:36:22 +0100

    Change cloudAMQP broker

--- client/src/main/scala/controller/TeamSelectionController.scala
+++ client/src/main/scala/controller/TeamSelectionController.scala
@@ -2,12 +2,15 @@ package controller
 
 import java.net.URL
 import java.util.ResourceBundle
+
 import javafx.event.ActionEvent
 import javafx.fxml.{FXML, Initializable}
 import view.ApplicationView
 import ApplicationView.viewSelector._
+import javafx.scene.control.Label
 
 class TeamSelectionController extends Initializable with ViewController {
+  @FXML var idLabel: Label = _
 
   @FXML def handleLogout(event: ActionEvent) {
     println("Logout pressed")
@@ -15,7 +18,9 @@ class TeamSelectionController extends Initializable with ViewController {
     ApplicationView changeView LOGIN
   }
 
-  override def initialize(location: URL, resources: ResourceBundle): Unit = {}
+  override def initialize(location: URL, resources: ResourceBundle): Unit = {
+    idLabel setText ("guest")
+  }
 }
 
 object TeamSelectionController {
