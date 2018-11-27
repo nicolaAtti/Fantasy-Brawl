871f64d28cb9a836fc02f29772932a2f1337deda
Daniele Schiavi <daniele.schiavi@studio.unibo.it>
2018-11-27 12:03:44 +0100

    Add selectable character

--- client/src/main/scala/controller/TeamSelectionController.scala
+++ client/src/main/scala/controller/TeamSelectionController.scala
@@ -9,7 +9,6 @@ import view.ApplicationView
 import ApplicationView.viewSelector._
 import javafx.geometry.Insets
 import javafx.scene.control.{Label, TextArea}
-import javafx.scene.image.ImageView
 import javafx.scene.input.MouseEvent
 import javafx.scene.layout._
 import javafx.scene.paint.Paint
@@ -20,11 +19,11 @@ object TeamSelectionController extends Initializable with ViewController {
 
   @FXML var idLabel: Label = _
   @FXML var characterDescription: TextArea = _
-  @FXML var selectedCharacter00: ImageView = _
+  @FXML var selectedCharacter00: StackPane = _
   @FXML var chosenCharacter0: StackPane = _
   @FXML var grid: GridPane = _
 
-  var selectedCharacter: ImageView = new ImageView()
+  var selectedCharacter: StackPane = new StackPane()
   var chosenCharacter: StackPane = new StackPane()
 
   @FXML def handleLogout(event: ActionEvent) {
@@ -34,13 +33,13 @@ object TeamSelectionController extends Initializable with ViewController {
   }
 
   @FXML def handleCharacterToChoosePressed(mouseEvent: MouseEvent) {
-    val characterPressed: ImageView = mouseEvent.getSource.asInstanceOf[ImageView]
+    val characterPressed: StackPane = mouseEvent.getSource.asInstanceOf[StackPane]
     println(characterPressed.getId + " pressed")
-    var pane: StackPane = selectedCharacter.getParent.asInstanceOf[StackPane]
-    pane.setBackground(new Background(new BackgroundFill(Paint.valueOf("WHITE"), CornerRadii.EMPTY, Insets.EMPTY)))
+    selectedCharacter.setBackground(
+      new Background(new BackgroundFill(Paint.valueOf("WHITE"), CornerRadii.EMPTY, Insets.EMPTY)))
     selectedCharacter = characterPressed
-    pane = selectedCharacter.getParent.asInstanceOf[StackPane]
-    pane.setBackground(new Background(new BackgroundFill(Paint.valueOf("BLUE"), CornerRadii.EMPTY, Insets.EMPTY)))
+    selectedCharacter.setBackground(
+      new Background(new BackgroundFill(Paint.valueOf("BLUE"), CornerRadii.EMPTY, Insets.EMPTY)))
     characterDescription.setText(characterPressed.getId)
 
   }
@@ -65,8 +64,8 @@ object TeamSelectionController extends Initializable with ViewController {
     })
     selectedCharacter = selectedCharacter00
     chosenCharacter = chosenCharacter0
-    val pane: StackPane = selectedCharacter.getParent.asInstanceOf[StackPane]
-    pane.setBackground(new Background(new BackgroundFill(Paint.valueOf("BLUE"), CornerRadii.EMPTY, Insets.EMPTY)))
+    selectedCharacter.setBackground(
+      new Background(new BackgroundFill(Paint.valueOf("BLUE"), CornerRadii.EMPTY, Insets.EMPTY)))
     chosenCharacter.setBackground(
       new Background(new BackgroundFill(Paint.valueOf("BLUE"), CornerRadii.EMPTY, Insets.EMPTY)))
   }
