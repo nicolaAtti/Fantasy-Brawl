1bdd193fe29cddb300918bb66d55c8f80c25724c
Nicola Atti <nicola.atti@studio.unibo.it>
2018-11-28 17:56:20 +0100

    Implement scala-prolog integration and related test

--- client/src/main/resources/model/PrologCharacters.pl
+++ client/src/main/resources/model/PrologCharacters.pl
@@ -8,3 +8,5 @@ character('Jacob','Warrior',51,33,13,5,27,['Skullcrack','Sismic Slam','Berserker
 character('Annabelle','Thief',34,46,22,8,27,['Poison Vial','Flash Grenade','Preparation','Backstab']).
 character('Albert','Healer',7,22,34,58,23,['Lay on Hands','Holy Smite','Censure','Bolster Faith']).
 character('Lidya','Wizard',19,11,33,54,27,['Flamestrike','Hypnosis','Freezing Wind','Concentration']).
+
+% Other characters will be added in the future
--- client/src/main/scala/model/Character.scala
+++ client/src/main/scala/model/Character.scala
@@ -5,29 +5,29 @@ trait Character {
   /**
     * The character's name
     */
-  var charName: String
+  var characterName: String
 
   /**
     * The character's base statistics
     */
-  var stats: Statistics
+  var statistics: Statistics
 
   /**
     * The character's class multipliers
     */
-  var classMods: ClassStat
+  var classMultipliers: ClassStat
 
   import utilities.Utility._
 
   /**
     * The character's maximum HP
     */
-  val maxHP: Int = roundDown((stats.resistance * classMods.hpMod) * 10)
+  val maxHP: Int = roundDown((statistics.resistance * classMultipliers.hpMod) * 10)
 
   /**
     * The character's maximum MP
     */
-  val maxMP: Int = roundDown((stats.spirit * classMods.spiMod) * 5)
+  val maxMP: Int = roundDown((statistics.spirit * classMultipliers.spiritMod) * 5)
 
   import scala.collection.mutable._
 
@@ -40,51 +40,51 @@ trait Character {
     * Calculates the character's physical damage
     * @return
     */
-  def calculatePhysDamage: Int = roundDown(stats.strength * classMods.strMod) + getModifierValues("PHYS_DAMAGE")
+  def calculatePhysicalDamage: Int = roundDown(statistics.strength * classMultipliers.strengthMod) + getModifierValues("PHYS_DAMAGE")
 
   /**
     * Calculates the character's bonus physical critical strike damage
     * @return
     */
-  def calculatePysCritDamage: Int =
-    roundDown((stats.strength * classMods.strMod) / 5 + 150) + getModifierValues("PHYS_DAMAGE")
+  def calculatePysicalCriticalDamage: Int =
+    roundDown((statistics.strength * classMultipliers.strengthMod) / 5 + 150) + getModifierValues("PHYS_DAMAGE")
 
   /**
     * Calculates the character's speed
     * @return
     */
-  def calculateSpeed: Int = roundDown((stats.agility * classMods.agiMod) / 10) + getModifierValues("PHYS_DAMAGE")
+  def calculateSpeed: Int = roundDown((statistics.agility * classMultipliers.agilityMod) / 10) + getModifierValues("PHYS_DAMAGE")
 
   /**
     * Calculates the character's critical strike chance
     * @return
     */
-  def calculateCritChance: Int = roundDown((stats.agility * classMods.agiMod) / 2) + getModifierValues("PHYS_DAMAGE")
+  def calculateCriticalChance: Int = roundDown((statistics.agility * classMultipliers.agilityMod) / 2) + getModifierValues("PHYS_DAMAGE")
 
   /**
     * Calculates the character's magical defence
     * @return
     */
-  def calculateMagDefence: Int = roundDown(stats.spirit * classMods.spiMod) + getModifierValues("PHYS_DAMAGE")
+  def calculateMagicalDefence: Int = roundDown(statistics.spirit * classMultipliers.spiritMod) + getModifierValues("PHYS_DAMAGE")
 
   /**
     * Calculates the character's magical damage
     * @return
     */
-  def calculateMagDamage: Int = roundDown(stats.intelligence * classMods.intMod) + getModifierValues("PHYS_DAMAGE")
+  def calculateMagicalDamage: Int = roundDown(statistics.intelligence * classMultipliers.intelligenceMod) + getModifierValues("PHYS_DAMAGE")
 
   /**
     * Calculates the character's bonus magical critical strike damage
     * @return
     */
-  def calculateMagicCritDamage: Int =
-    roundDown((stats.intelligence * classMods.intMod) / 10 + 150) + getModifierValues("PHYS_DAMAGE")
+  def calculateMagicalCriticalDamage: Int =
+    roundDown((statistics.intelligence * classMultipliers.intelligenceMod) / 10 + 150) + getModifierValues("PHYS_DAMAGE")
 
   /**
     * Calculates the character's physical defence
     * @return
     */
-  def calculatePhysDefence: Int = roundDown(stats.resistance * classMods.resMod) + getModifierValues("PHYS_DAMAGE")
+  def calculatePhysicalDefence: Int = roundDown(statistics.resistance * classMultipliers.resistanceMod) + getModifierValues("PHYS_DAMAGE")
 
   //Passagli la funzione della mossa, sia essa speciale, attacco base o passare il turno   STRATEGY
   //Forse conviene mettere qui il controllo per le afflizzioni che limitano le mosse tipo: if stunnato passa solo, if ghiacciato no mosse meelee etc
@@ -94,8 +94,8 @@ trait Character {
   /**
     * Resets the status of the character to the original values
     */
-  def resetStatus(): Unit = {
-    status = Status(maxHP, maxMP, MutableList(), MutableList())
+  def setStatus(newStatus: Status): Unit = {
+    status = newStatus
   }
 
   /**
@@ -106,7 +106,7 @@ trait Character {
     */
   def getModifierValues(affectedStat: String): Int = {
     var allModsValue = 0
-    status.modifiers foreach (modifier => allModsValue = add(allModsValue, modifier.modValue))
+    status.modifiers foreach (modifier => allModsValue = add(allModsValue, modifier.modifierValue))
     allModsValue
   }
 
@@ -115,39 +115,39 @@ trait Character {
 /**
   * Default implementation of a game Character
   *
-  * @param charName  the name of the Character
-  * @param stats     the statistics of the Character
-  * @param classMods the class statistic modifier, this will vary from Character implementation
+  * @param characterName  the name of the Character
+  * @param statistics     the statistics of the Character
+  * @param classMultipliers the class statistic modifier, this will vary from Character implementation
   * @author Nicola Atti
   */
-private case class Warrior(override var charName: String,
-                           override var stats: Statistics,
-                           override var classMods: ClassStat = ClassStat(2, 1, 1, 0.5, 1.5, 2))
+private class Warrior(override var characterName: String,
+                           override var statistics: Statistics,
+                           override var classMultipliers: ClassStat = ClassStat(2, 1, 1, 0.5, 1.5, 2))
     extends Character {}
 
-private case class Thief(override var charName: String,
-                         override var stats: Statistics,
-                         override var classMods: ClassStat = ClassStat(1.5, 2, 1, 0.5, 1, 1.5))
+private case class Thief(override var characterName: String,
+                         override var statistics: Statistics,
+                         override var classMultipliers: ClassStat = ClassStat(1.5, 2, 1, 0.5, 1, 1.5))
     extends Character {}
 
-private case class Wizard(override var charName: String,
-                          override var stats: Statistics,
-                          override var classMods: ClassStat = ClassStat(1, 1, 1.5, 2, 0.5, 1.5))
+private case class Wizard(override var characterName: String,
+                          override var statistics: Statistics,
+                          override var classMultipliers: ClassStat = ClassStat(1, 1, 1.5, 2, 0.5, 1.5))
     extends Character {}
 
-private case class Healer(override var charName: String,
-                          override var stats: Statistics,
-                          override var classMods: ClassStat = ClassStat(1.5, 0.5, 1, 2, 1, 1.5))
+private case class Healer(override var characterName: String,
+                          override var statistics: Statistics,
+                          override var classMultipliers: ClassStat = ClassStat(1.5, 0.5, 1, 2, 1, 1.5))
     extends Character {}
 
 object Character {
 
-  def apply(role: String, charName: String, stats: Statistics): Character =
+  def apply(role: String, characterName: String, statistics: Statistics): Character =
     role match {
-      case "Warrior" => Warrior(charName, stats)
-      case "Thief"   => Thief(charName, stats)
-      case "Wizard"  => Wizard(charName, stats)
-      case "Healer"  => Healer(charName, stats)
+      case "Warrior" => new Warrior(characterName, statistics)
+      case "Thief"   => Thief(characterName, statistics)
+      case "Wizard"  => Wizard(characterName, statistics)
+      case "Healer"  => Healer(characterName, statistics)
     }
 
 }
