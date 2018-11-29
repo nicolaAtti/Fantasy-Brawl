479b2a779f26bfdaa0c5a4f6a7793238cbae0aa9
Marco Canducci <marco.candoz@gmail.com>
2018-11-29 22:39:43 +0100

    Fix in Character and some minor renamings
    There were remains from a previous copy-paste in Character, that has been
    fixed and substituted with proper case objects instead of strings.
    It has been refactored and fixed the getModiferValues method.
    Some minor parameter renaming in ImmutableStatus and ImmutableModifier.

--- client/src/main/scala/model/Character.scala
+++ client/src/main/scala/model/Character.scala
@@ -26,74 +26,81 @@ trait Character {
   private def calculateMaxManaPoints(): Int = {
     roundDown((this.statistics.spirit * this.classMultipliers.spiritMod) * 5)
   }
-  
+
+  import SubStatistic._
 
   /**
     * Calculates the character's physical damage
     * @return
     */
   def physicalDamage: Int =
-    roundDown(statistics.strength * classMultipliers.strengthMod) + getModifierValues("PHYS_DAMAGE")
+    roundDown(statistics.strength * classMultipliers.strengthMod) + getModifierValues(PhysicalDamage)
 
   /**
     * Calculates the character's bonus physical critical strike damage
     * @return
     */
   def pysicalCriticalDamage: Int =
-    roundDown((statistics.strength * classMultipliers.strengthMod) / 5 + 150) + getModifierValues("PHYS_DAMAGE")
+    roundDown((statistics.strength * classMultipliers.strengthMod) / 5 + 150) + getModifierValues(
+      PhysicalCriticalDamage)
 
   /**
     * Calculates the character's speed
     * @return
     */
   def speed: Int =
-    roundDown((statistics.agility * classMultipliers.agilityMod) / 10) + getModifierValues("PHYS_DAMAGE")
+    roundDown((statistics.agility * classMultipliers.agilityMod) / 10) + getModifierValues(Speed)
 
   /**
     * Calculates the character's critical strike chance
     * @return
     */
   def criticalChance: Int =
-    roundDown((statistics.agility * classMultipliers.agilityMod) / 2) + getModifierValues("PHYS_DAMAGE")
+    roundDown((statistics.agility * classMultipliers.agilityMod) / 2) + getModifierValues(CriticalChance)
 
   /**
     * Calculates the character's magical defence
     * @return
     */
   def magicalDefence: Int =
-    roundDown(statistics.spirit * classMultipliers.spiritMod) + getModifierValues("PHYS_DAMAGE")
+    roundDown(statistics.spirit * classMultipliers.spiritMod) + getModifierValues(MagicalDefence)
 
   /**
     * Calculates the character's magical damage
     * @return
     */
   def magicalDamage: Int =
-    roundDown(statistics.intelligence * classMultipliers.intelligenceMod) + getModifierValues("PHYS_DAMAGE")
+    roundDown(statistics.intelligence * classMultipliers.intelligenceMod) + getModifierValues(MagicalDamage)
 
   /**
     * Calculates the character's bonus magical critical strike damage
     * @return
     */
   def magicalCriticalDamage: Int =
-    roundDown((statistics.intelligence * classMultipliers.intelligenceMod) / 10 + 150) + getModifierValues("PHYS_DAMAGE")
+    roundDown((statistics.intelligence * classMultipliers.intelligenceMod) / 10 + 150) + getModifierValues(
+      MagicalCriticalDamage)
 
   /**
     * Calculates the character's physical defence
     * @return
     */
   def physicalDefence: Int =
-    roundDown(statistics.resistance * classMultipliers.resistanceMod) + getModifierValues("PHYS_DAMAGE")
+    roundDown(statistics.resistance * classMultipliers.resistanceMod) + getModifierValues(PhysicalDefence)
 
   /**
     * Cycles all the character's modifiers for a given sub-statistic and calculates the bonus/malus value
-    * @param affectedStat the sub-statistic to search for
+    * @param subStatistic the sub-statistic to search for
     * @return the total bonus/malus to the sub-statistic
     * @author Nicola Atti
     */
-  private def getModifierValues(affectedStat: String): Int = {
-    var allModsValue = 0
-    status.modifiers foreach (modifier => allModsValue = add(allModsValue, modifier._2.value))
-    allModsValue
+  private def getModifierValues(subStatistic: SubStatistic): Int = {
+//    var allModsValue = 0
+//    status.modifiers foreach (modifier => allModsValue = add(allModsValue, modifier._2.value))
+//    allModsValue
+    status.modifiers
+      .filter(kv => kv._2.affectsSubStatistic == subStatistic)
+      .map(kv => kv._2.delta)
+      .sum
   }
 
 }
--- client/src/main/scala/model/ImmutableStatus.scala
+++ client/src/main/scala/model/ImmutableStatus.scala
@@ -10,8 +10,12 @@ case class ImmutableStatus(healthPoints: Int,
 object ImmutableStatus {
 
   def nextRound(s: ImmutableStatus): ImmutableStatus =
-    s.copy(modifiers = s.modifiers.mapValues(v => v.copy(value = v.value - 1)).filter(kv => kv._2.value > 0),
-           afflictions = s.afflictions.mapValues(v => v - 1).filter(kv => kv._2 > 0))
+    s.copy(
+      modifiers = s.modifiers
+        .mapValues(v => v.copy(roundsDuration = v.roundsDuration - 1))
+        .filter(kv => kv._2.roundsDuration > 0),
+      afflictions = s.afflictions.mapValues(v => v - 1).filter(kv => kv._2 > 0)
+    )
 
   def consumingMana(s: ImmutableStatus, mp: Int): ImmutableStatus =
     s.copy(manaPoints = s.manaPoints - mp)
