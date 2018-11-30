2f3719de213794c489b1a8f53856ecb176f55276
Daniele Schiavi <daniele.schiavi@studio.unibo.it>
2018-11-30 15:58:29 +0100

    Add 4 character and the functionality to set the description when selected

--- client/src/main/scala/controller/TeamSelectionController.scala
+++ client/src/main/scala/controller/TeamSelectionController.scala
@@ -7,6 +7,7 @@ import javafx.event.ActionEvent
 import javafx.fxml.{FXML, Initializable}
 import view.ApplicationView
 import ApplicationView.viewSelector._
+import alice.tuprolog.SolveInfo
 import javafx.geometry.Insets
 import javafx.scene.control.{Label, TextArea}
 import javafx.scene.image.ImageView
@@ -25,7 +26,7 @@ object TeamSelectionController extends Initializable with ViewController {
 
   @FXML var idLabel: Label = _
   @FXML var characterDescription: TextArea = _
-  @FXML var selectedCharacter00: StackPane = _
+  @FXML var Jacob: StackPane = _
   @FXML var chosenCharacter0: StackPane = _
   @FXML var gridChosen: GridPane = _
   @FXML var gridToChoose: GridPane = _
@@ -47,13 +48,7 @@ object TeamSelectionController extends Initializable with ViewController {
     */
   @FXML def handleCharacterToChoosePressed(mouseEvent: MouseEvent) {
     val characterPressed: StackPane = mouseEvent.getSource.asInstanceOf[StackPane]
-    println(characterPressed.getId + " pressed")
-    changeSelectedCharacter(selectedCharacter, characterPressed)
-    chosenCharacter.getChildren
-      .get(0)
-      .asInstanceOf[ImageView]
-      .setImage(selectedCharacter.getChildren.get(0).asInstanceOf[ImageView].getImage)
-    characterDescription.setText(characterPressed.getId)
+    changeCharacterToChoose(selectedCharacter, characterPressed)
   }
 
   /**
@@ -61,34 +56,66 @@ object TeamSelectionController extends Initializable with ViewController {
     */
   @FXML def handleCharacterChosenPressed(mouseEvent: MouseEvent) {
     val characterPressed: StackPane = mouseEvent.getSource.asInstanceOf[StackPane]
-    println(characterPressed.getId + " pressed")
-    changeSelectedCharacter(chosenCharacter, characterPressed)
-    characterDescription.setText(characterPressed.getId)
+    changeCharacterChosen(chosenCharacter, characterPressed)
   }
 
   override def initialize(location: URL, resources: ResourceBundle): Unit = {
     idLabel setText username
-    deselectAllCharacters(gridChosen)
-    deselectAllCharacters(gridToChoose)
-    selectedCharacter = selectedCharacter00
+    deselectAllCharacters(gridChosen, gridToChoose)
     chosenCharacter = chosenCharacter0
-    selectCharacter(selectedCharacter)
+    selectedCharacter = Jacob
     selectCharacter(chosenCharacter)
+    changeCharacterToChoose(selectedCharacter, Jacob)
   }
 
-  private def deselectAllCharacters(gridPane: GridPane): Unit = {
-    gridPane.getChildren.forEach(pane => {
-      deselectCharacter(pane.asInstanceOf[StackPane])
+  private def setDescription(characterName: String): Unit = {
+    import utilities.ScalaProlog._
+    val solveInfo: SolveInfo = getCharacter(characterName)
+    var description: String = "Character name: " + characterName + "\n" +
+      "Character class: " + extractString(solveInfo, "Class") + "\n" +
+      "  Strength: " + extractInt(solveInfo, "Strength") + "\n" +
+      "  Agility: " + extractInt(solveInfo, "Agility") + "\n" +
+      "  Spirit: " + extractInt(solveInfo, "Spirit") + "\n" +
+      "  Intelligence: " + extractInt(solveInfo, "Intelligence") + "\n" +
+      "  Resistence: " + extractInt(solveInfo, "Resistence") + "\n" +
+      "  MoveList: \n"
+    extractList(solveInfo, "MoveList").foreach(move => {
+      description += "    " + move +
+        " -> Type: " + extractString(getMove(move), "Type") +
+        ", Base damage: " + extractInt(getMove(move), "BaseValue") +
+        ", Mana cost: " + extractInt(getMove(move), "MPCost") +
+        "\n"
     })
+    characterDescription.setText(description)
   }
 
-  private def changeSelectedCharacter(previous: StackPane, next: StackPane): Unit = {
+  private def deselectAllCharacters(gridPanes: GridPane*): Unit = {
+    gridPanes.foreach(gridPane => gridPane.getChildren.forEach(pane => {
+      deselectCharacter(pane.asInstanceOf[StackPane])
+    }))
+    /*gridPane.getChildren.forEach(pane => {
+      deselectCharacter(pane.asInstanceOf[StackPane])
+    })*/
+  }
+
+  private def changeCharacterToChoose(previous: StackPane, next: StackPane): Unit = {
+    changeCharacter(previous, next)
+    selectedCharacter = next
+    chosenCharacter.getChildren
+      .get(0)
+      .asInstanceOf[ImageView]
+      .setImage(selectedCharacter.getChildren.get(0).asInstanceOf[ImageView].getImage)
+    setDescription(selectedCharacter.getId)
+  }
+
+  private def changeCharacterChosen(previous: StackPane, next: StackPane): Unit = {
+    changeCharacter(previous, next)
+    chosenCharacter = next
+  }
+
+  private def changeCharacter(previous: StackPane, next: StackPane): Unit = {
     deselectCharacter(previous)
     selectCharacter(next)
-    if (previous == chosenCharacter)
-      chosenCharacter = next
-    else
-      selectedCharacter = next
   }
 
   private def selectCharacter(stackPane: StackPane): Unit = {
