d120003275c4c9600d2e9111a646517ccd336bd2
Daniele Schiavi <daniele.schiavi@studio.unibo.it>
2018-12-03 17:32:01 +0100

    Change doc

--- shared/src/main/scala/communication/JoinCasualQueueResponse.scala
+++ shared/src/main/scala/communication/JoinCasualQueueResponse.scala
@@ -1,9 +1,7 @@
 package communication
 
-/** The response message that the client will receive after a LoginGuestRequest.
+/** The response message that the client will receive after an opponent is chosen from the matchmaking service.
   *
-  * @param guestId the incremental ID assigned to the guest
-  * @param details eventual details about the response that the server might attach for the client
-  * @author Marco Canducci
+  * @param enemyTeam team with which the opponent player wants to fight.
   */
 case class JoinCasualQueueResponse(enemyTeam: Option[Map[String, String]], details: Option[String])
