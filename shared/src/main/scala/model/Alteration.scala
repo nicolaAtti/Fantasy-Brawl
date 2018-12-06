2617efdced883731a08dc728406a5c03e5f006cd
Marco Canducci <marco.candoz@gmail.com>
2018-12-06 16:32:44 +0100

    Add turnDuration in Alterations

--- shared/src/main/scala/model/Alteration.scala
+++ shared/src/main/scala/model/Alteration.scala
@@ -2,6 +2,7 @@ package model
 
 sealed trait Alteration {
   def inhibits(move: Move): Boolean
+  def turnDuration: Int
   def beginTurnStatusVariation: Option[(Status) => Status]
 }
 
@@ -11,46 +12,54 @@ object Alteration {
   val regenerationMultiplier = 1.25
 
   case object Stunned extends Alteration {
-    override def inhibits(move: Move): Boolean = true
+    override def inhibits(move: Move) = true
+    override val turnDuration = 1
     override val beginTurnStatusVariation = None
   }
 
   case object Asleep extends Alteration {
-    override def inhibits(move: Move): Boolean = true
+    override def inhibits(move: Move) = true
+    override val turnDuration = 3
     override val beginTurnStatusVariation = None
   }
 
   case object Poisoned extends Alteration {
-    override def inhibits(move: Move): Boolean = false
+    override def inhibits(move: Move) = false
+    override val turnDuration = 3
     override val beginTurnStatusVariation =
       Some(s => s.copy(healthPoints = Math.ceil(s.healthPoints * poisonedMultiplier).toInt)) // cannot die from poisoning
   }
 
   case object Regeneration extends Alteration {
-    override def inhibits(move: Move): Boolean = false
+    override def inhibits(move: Move) = false
+    override val turnDuration = 2
     override val beginTurnStatusVariation: Option[Status => Status] =
       Some(s => s.copy(healthPoints = Math.round(s.healthPoints * regenerationMultiplier).toInt min s.maxHealthPoints))
   }
 
   case object Berserk extends Alteration {
-    override def inhibits(move: Move): Boolean = move.isInstanceOf[SpecialMove]
+    override def inhibits(move: Move) = move.isInstanceOf[SpecialMove]
+    override val turnDuration = 3
     override val beginTurnStatusVariation = None
   }
 
   import MoveType._
 
   case object Silenced extends Alteration {
-    override def inhibits(move: Move): Boolean = move.moveType == Spell
+    override def inhibits(move: Move) = move.moveType == Spell
+    override val turnDuration = 2
     override val beginTurnStatusVariation = None
   }
 
   case object Frozen extends Alteration {
-    override def inhibits(move: Move): Boolean = move.moveType == Melee
+    override def inhibits(move: Move) = move.moveType == Melee
+    override val turnDuration = 2
     override val beginTurnStatusVariation = None
   }
 
   case object Blinded extends Alteration {
-    override def inhibits(move: Move): Boolean = move.moveType == Ranged
+    override def inhibits(move: Move) = move.moveType == Ranged
+    override val turnDuration = 2
     override val beginTurnStatusVariation = None
   }
 
