899c1e516bd44991bfa75debe169acf776c3a6bd
Daniele Schiavi <daniele.schiavi@studio.unibo.it>
2018-12-07 21:54:26 +0100

    Refactor test of character and ScalaProlog utils

--- shared/src/test/scala/model/CharacterTest.scala
+++ shared/src/test/scala/model/CharacterTest.scala
@@ -1,7 +1,6 @@
 package model
 
 import org.scalatest.FunSuite
-import utilities.Utility._
 
 /** *
   * Test for the Character class, and classes composing it
@@ -14,36 +13,39 @@ class CharacterTest extends FunSuite {
   private val jacob = Character("Warrior", "Jacob", jacobStats)
   private val annabelle = Character("Thief", "Annabelle", annabelleStats)
 
-  test("test character creation") {
-
-    assert(jacob.characterName equals "Jacob")
-
-    assert(jacob.statistics.strength == 51)
-    assert(jacob.statistics.agility == 33)
-    assert(jacob.statistics.spirit == 13)
-    assert(jacob.statistics.intelligence == 5)
-    assert(jacob.statistics.resistance == 27)
+  test("New character should have his name and statistics at the creation") {
+    assert(
+      jacob.characterName.equals("Jacob") &&
+        jacob.statistics.strength == 51 &&
+        jacob.statistics.agility == 33 &&
+        jacob.statistics.spirit == 13 &&
+        jacob.statistics.intelligence == 5 &&
+        jacob.statistics.resistance == 27)
   }
-  test("test the right calculation of sub-statistics") {
-    assert(jacob.physicalDamage == 102)
-    assert(jacob.physicalCriticalDamage == 170)
+  test("New character should have the right sub-statistics") {
+    assert(
+      jacob.physicalDamage == 102 &&
+        jacob.physicalCriticalDamage == 170 &&
+        jacob.speed == 4 &&
+        jacob.criticalChance == 16 &&
+        jacob.magicalDefence == 13 &&
+        jacob.status.maxManaPoints == 65 &&
+        jacob.magicalPower == 2 &&
+        jacob.magicalCriticalPower == 150 &&
+        jacob.status.maxHealthPoints == 540 &&
+        jacob.physicalDefence == 40)
+  }
 
-    assert(jacob.speed == 3)
-    assert(jacob.criticalChance == 16)
-
-    assert(jacob.magicalDefence == 13)
-    assert(jacob.status.maxManaPoints == 65)
-
-    assert(jacob.magicalPower == 2)
-    assert(jacob.magicalCriticalPower == 150)
-
-    assert(jacob.status.maxHealthPoints == 540)
-    assert(jacob.physicalDefence == 40)
+  test("New character should not have modifiers or alterations") {
+    assert(
+      jacob.status.modifiers.isEmpty &&
+        jacob.status.alterations.isEmpty)
   }
 
   test("The initial health and mp should be equal to it's maximum value when created") {
-    assert(jacob.status.healthPoints == jacob.status.maxHealthPoints) //540
-    assert(jacob.status.manaPoints == jacob.status.maxManaPoints) //65
+    assert(
+      jacob.status.healthPoints == jacob.status.maxHealthPoints && //540
+        jacob.status.manaPoints == jacob.status.maxManaPoints) //65
   }
 
   test("Different character should have a different name") {
@@ -51,19 +53,16 @@ class CharacterTest extends FunSuite {
   }
 
   test("Other classes should have the right statistics") {
-    assert(annabelle.physicalDamage == 51)
-    assert(annabelle.physicalCriticalDamage == 160)
-
-    assert(annabelle.speed == 9)
-    assert(annabelle.criticalChance == 46)
-
-    assert(annabelle.magicalDefence == 22)
-    assert(annabelle.status.maxManaPoints == 110)
-
-    assert(annabelle.magicalPower == 4)
-    assert(annabelle.magicalCriticalPower == 150)
-
-    assert(annabelle.status.maxHealthPoints == 405)
-    assert(annabelle.physicalDefence == 27)
+    assert(
+      annabelle.physicalDamage == 51 &&
+        annabelle.physicalCriticalDamage == 160 &&
+        annabelle.speed == 10 &&
+        annabelle.criticalChance == 46 &&
+        annabelle.magicalDefence == 22 &&
+        annabelle.status.maxManaPoints == 110 &&
+        annabelle.magicalPower == 4 &&
+        annabelle.magicalCriticalPower == 150 &&
+        annabelle.status.maxHealthPoints == 405 &&
+        annabelle.physicalDefence == 27)
   }
 }
--- shared/src/test/scala/model/ScalaPrologTest.scala
+++ shared/src/test/scala/model/ScalaPrologTest.scala
@@ -1,44 +1,50 @@
 package model
 
-import alice.tuprolog.NoMoreSolutionException
 import org.scalatest.FunSuite
 import utilities.ScalaProlog._
 
 class ScalaPrologTest extends FunSuite {
-  private val charList: List[String] = List("Jacob","Annabelle","Albert","Lidya")
-  private val jacobMoveList: List[String] = List("Skullcrack","Sismic Slam","Berserker Rage","Second Wind")
-  private val spellList: List[String] = List("Lay On Hands","Censure","Bolster Faith","Preparation","Second Wind","Berserker Rage","Freezing Wind","Hypnosis","Concentration","Flamestrike")
+  private val charList: List[String] =
+    List("Jacob", "Annabelle", "Albert", "Lidya", "Cassandra", "Noah", "Fernando", "Nora")
+  private val jacobMoveList: List[String] =
+    List("Physical Attack", "Skullcrack", "Sismic Slam", "Berserker Rage", "Second Wind")
+  private val spellList: List[String] = List("Lay On Hands",
+                                             "Censure",
+                                             "Bolster Faith",
+                                             "Preparation",
+                                             "Second Wind",
+                                             "Berserker Rage",
+                                             "Freezing Wind",
+                                             "Hypnosis",
+                                             "Concentration",
+                                             "Flamestrike")
 
   test("Get all facts and extract a String from each of them") {
-    try {
-      getAllCharacters() foreach (sol => assert(charList.contains(extractString(sol,"Name"))))
-    } catch {
-      case ex: NoMoreSolutionException =>
-    }
+    assert(
+      getAllCharacters()
+        .map(character => extractString(character, "Name"))
+        .toList == charList)
   }
 
-  test("Extract an Int from a prolog fact"){
-    try {
-      assert(extractInt(getMove("Skullcrack"),"BaseValue") == 30)
-      assert(extractInt(getCharacter("Jacob"),"Strength") == 51)
-    } catch {
-      case ex: NoMoreSolutionException =>
-    }
+  test("Extract an Int from a prolog fact of a move") {
+    assert(extractInt(getMove("Skullcrack"), "BaseValue") == 30)
   }
-  test("Extract a List from a prolog fact"){
-    try {
-      assert(extractList(getCharacter("Jacob"),"MoveList") == jacobMoveList)
-    } catch {
-      case ex: NoMoreSolutionException =>
-    }
+  test("Extract an Int from a prolog fact of a character") {
+    assert(extractInt(getCharacter("Jacob"), "Strength") == 51)
   }
-  test("Get all possible results given a custom goal and extract a value"){
+  test("Extract a List from a prolog fact") {
+    assert(extractList(getCharacter("Jacob"), "MoveList") == jacobMoveList)
+  }
+  test("Get all possible results given a custom goal and extract a value") {
     setNewTheory(moveContents)
-    try {
-      solveN("spec_move(MoveName,'Spell',BaseDamage,ManaCost,ModifiersList,AfflictionsList,NOfTargets).") foreach(sol => assert(spellList.contains(extractString(sol,"MoveName"))))
-    }catch{
-      case ex: NoMoreSolutionException =>
-    }
+    val allMoves = solveN("move(MoveName,DamageType,Type,BaseValue,Mods,Affls,RemovedAfflictions,MPCost,NTargets).")
+      .map(move => extractString(move, "MoveName"))
+      .toList
+    assert(
+      spellList
+        .map(moveName => allMoves contains moveName)
+        .forall(_ == true))
+
   }
 
 }
