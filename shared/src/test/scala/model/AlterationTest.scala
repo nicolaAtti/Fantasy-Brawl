f1d8a337c252bb6d6cdec5cd40a3f6d332dee079
Daniele Schiavi <daniele.schiavi@studio.unibo.it>
2018-12-10 15:40:26 +0100

    Refactor alteration test

--- shared/src/test/scala/model/AlterationTest.scala
+++ shared/src/test/scala/model/AlterationTest.scala
@@ -3,28 +3,132 @@ package model
 import org.scalatest.FunSuite
 
 class AlterationTest extends FunSuite {
-  val jacobStats = Statistics(51, 33, 13, 5, 27)
-  val jacob = Character("Warrior", "Jacob", jacobStats)
 
-  //var stunned = ImmutableAffliction("Stunned", 1)
-  //var poisoned = ImmutableAffliction("Poisoned", 3)
-  //var berserk = ImmutableAffliction("Berserk", 3)
-  //var silenced = ImmutableAffliction("Silenced", 2)
-  //var frozen = ImmutableAffliction("Frozen", 2)
-  //var asleep = ImmutableAffliction("Asleep", 3)
-  //var blinded = ImmutableAffliction("Blinded", 2)
-
-  test("The afflictions must be correctly istantiated") {
-    //assert(stunned.afflictionType equals ("Stunned"))
-    //assert(stunned.turnDuration == 1)
-
-    //assert(poisoned.afflictionType equals ("Poisoned"))
-    //assert(poisoned.turnDuration == 3)
+  val stunned = Alteration("Stunned")
+  test("The stunned alteration should inhibit all moves") {
+    assert(
+      stunned.inhibits(PhysicalAttack) &&
+        stunned.inhibits(SpecialMove("StandardDamage", MoveType("Melee"), 0, Map(), Map(), Set(), 0, 1)) &&
+        stunned.inhibits(SpecialMove("StandardHeal", MoveType("Spell"), 0, Map(), Map(), Set(), 0, 1)) &&
+        stunned.inhibits(SpecialMove("Percentage", MoveType("Ranged"), 0, Map(), Map(), Set(), 0, 1)) &&
+        stunned.inhibits(SpecialMove("BuffDebuff", MoveType("Spell"), 0, Map(), Map(), Set(), 0, 1)))
   }
-  /*test("After a round the duration of the affliction should be reduced"){
-    jacob.status = jacob.status.copy(afflictions = List(poisoned,stunned))
-    jacob.status = ImmutableStatus.afterRound(jacob.status)
+  test("The stunned alteration should last 1 turn") {
+    assert(stunned.turnDuration == 1)
+  }
+  test("The stunned alteration should not cause status variations") {
+    assert(stunned.beginTurnStatusVariation.isEmpty)
+  }
 
-    assert(!(jacob.status.afflictions.contains()))
-  }*/
+  val asleep = Alteration("Asleep")
+  test("The asleep alteration should inhibit all moves") {
+    assert(
+      asleep.inhibits(PhysicalAttack) &&
+        asleep.inhibits(SpecialMove("StandardDamage", MoveType("Melee"), 0, Map(), Map(), Set(), 0, 1)) &&
+        asleep.inhibits(SpecialMove("StandardHeal", MoveType("Spell"), 0, Map(), Map(), Set(), 0, 1)) &&
+        asleep.inhibits(SpecialMove("Percentage", MoveType("Ranged"), 0, Map(), Map(), Set(), 0, 1)) &&
+        asleep.inhibits(SpecialMove("BuffDebuff", MoveType("Spell"), 0, Map(), Map(), Set(), 0, 1)))
+  }
+  test("The asleep alteration should last 3 turns") {
+    assert(asleep.turnDuration == 3)
+  }
+  test("The asleep alteration should not cause status variations") {
+    assert(asleep.beginTurnStatusVariation.isEmpty)
+  }
+
+  val poisoned = Alteration("Poisoned")
+  test("The poisoned alteration should not inhibit any move") {
+    assert(
+      !poisoned.inhibits(PhysicalAttack) &&
+        !poisoned.inhibits(SpecialMove("StandardDamage", MoveType("Melee"), 0, Map(), Map(), Set(), 0, 1)) &&
+        !poisoned.inhibits(SpecialMove("StandardHeal", MoveType("Spell"), 0, Map(), Map(), Set(), 0, 1)) &&
+        !poisoned.inhibits(SpecialMove("Percentage", MoveType("Ranged"), 0, Map(), Map(), Set(), 0, 1)) &&
+        !poisoned.inhibits(SpecialMove("BuffDebuff", MoveType("Spell"), 0, Map(), Map(), Set(), 0, 1)))
+  }
+  test("The poisoned alteration should last 3 turns") {
+    assert(poisoned.turnDuration == 3)
+  }
+  test("The poisoned alteration should cause status variations") {
+    assert(poisoned.beginTurnStatusVariation.isDefined)
+  }
+
+  val regeneration = Alteration("Regeneration")
+  test("The regeneration alteration should not inhibit any move") {
+    assert(
+      !regeneration.inhibits(PhysicalAttack) &&
+        !regeneration.inhibits(SpecialMove("StandardDamage", MoveType("Melee"), 0, Map(), Map(), Set(), 0, 1)) &&
+        !regeneration.inhibits(SpecialMove("StandardHeal", MoveType("Spell"), 0, Map(), Map(), Set(), 0, 1)) &&
+        !regeneration.inhibits(SpecialMove("Percentage", MoveType("Ranged"), 0, Map(), Map(), Set(), 0, 1)) &&
+        !regeneration.inhibits(SpecialMove("BuffDebuff", MoveType("Spell"), 0, Map(), Map(), Set(), 0, 1)))
+  }
+  test("The regeneration alteration should last 3 turns") {
+    assert(regeneration.turnDuration == 2)
+  }
+  test("The regeneration alteration should cause status variations") {
+    assert(regeneration.beginTurnStatusVariation.isDefined)
+  }
+
+  val berserk = Alteration("Berserk")
+  test("The berserk alteration should inhibit all moves except the physical attack") {
+    assert(
+      !berserk.inhibits(PhysicalAttack) &&
+        berserk.inhibits(SpecialMove("StandardDamage", MoveType("Melee"), 0, Map(), Map(), Set(), 0, 1)) &&
+        berserk.inhibits(SpecialMove("StandardHeal", MoveType("Spell"), 0, Map(), Map(), Set(), 0, 1)) &&
+        berserk.inhibits(SpecialMove("Percentage", MoveType("Ranged"), 0, Map(), Map(), Set(), 0, 1)) &&
+        berserk.inhibits(SpecialMove("BuffDebuff", MoveType("Spell"), 0, Map(), Map(), Set(), 0, 1)))
+  }
+  test("The berserk alteration should last 3 turns") {
+    assert(berserk.turnDuration == 3)
+  }
+  test("The berserk alteration should not cause status variations") {
+    assert(berserk.beginTurnStatusVariation.isEmpty)
+  }
+
+  val silenced = Alteration("Silenced")
+  test("The silenced alteration should inhibit only spell moves") {
+    assert(
+      silenced.inhibits(PhysicalAttack) &&
+        silenced.inhibits(SpecialMove("StandardDamage", MoveType("Melee"), 0, Map(), Map(), Set(), 0, 1)) &&
+        !silenced.inhibits(SpecialMove("StandardHeal", MoveType("Spell"), 0, Map(), Map(), Set(), 0, 1)) &&
+        silenced.inhibits(SpecialMove("Percentage", MoveType("Ranged"), 0, Map(), Map(), Set(), 0, 1)) &&
+        !silenced.inhibits(SpecialMove("BuffDebuff", MoveType("Spell"), 0, Map(), Map(), Set(), 0, 1)))
+  }
+  test("The silenced alteration should last 2 turns") {
+    assert(silenced.turnDuration == 2)
+  }
+  test("The silenced alteration should not cause status variations") {
+    assert(silenced.beginTurnStatusVariation.isEmpty)
+  }
+
+  val frozen = Alteration("Frozen")
+  test("The frozen alteration should inhibit only melee moves") {
+    assert(
+      frozen.inhibits(PhysicalAttack) &&
+        frozen.inhibits(SpecialMove("StandardDamage", MoveType("Melee"), 0, Map(), Map(), Set(), 0, 1)) &&
+        !frozen.inhibits(SpecialMove("StandardHeal", MoveType("Spell"), 0, Map(), Map(), Set(), 0, 1)) &&
+        !frozen.inhibits(SpecialMove("Percentage", MoveType("Ranged"), 0, Map(), Map(), Set(), 0, 1)) &&
+        !frozen.inhibits(SpecialMove("BuffDebuff", MoveType("Spell"), 0, Map(), Map(), Set(), 0, 1)))
+  }
+  test("The frozen alteration should last 2 turns") {
+    assert(frozen.turnDuration == 2)
+  }
+  test("The frozen alteration should not cause status variations") {
+    assert(frozen.beginTurnStatusVariation.isEmpty)
+  }
+
+  val blinded = Alteration("Blinded")
+  test("The blinded alteration should inhibit only ranged moves") {
+    assert(
+      !blinded.inhibits(PhysicalAttack) &&
+        !blinded.inhibits(SpecialMove("StandardDamage", MoveType("Melee"), 0, Map(), Map(), Set(), 0, 1)) &&
+        !blinded.inhibits(SpecialMove("StandardHeal", MoveType("Spell"), 0, Map(), Map(), Set(), 0, 1)) &&
+        blinded.inhibits(SpecialMove("Percentage", MoveType("Ranged"), 0, Map(), Map(), Set(), 0, 1)) &&
+        !blinded.inhibits(SpecialMove("BuffDebuff", MoveType("Spell"), 0, Map(), Map(), Set(), 0, 1)))
+  }
+  test("The blinded alteration should last 2 turns") {
+    assert(blinded.turnDuration == 2)
+  }
+  test("The blinded alteration should not cause status variations") {
+    assert(blinded.beginTurnStatusVariation.isEmpty)
+  }
 }
