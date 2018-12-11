506e6d5c1bd13cd667efc528a20e719e45bfa11f
Daniele Schiavi <daniele.schiavi@studio.unibo.it>
2018-12-11 22:36:17 +0100

    Refactor scalaProlog test

--- shared/src/test/scala/model/ScalaPrologTest.scala
+++ shared/src/test/scala/model/ScalaPrologTest.scala
@@ -4,13 +4,16 @@ import org.scalatest.FunSuite
 import utilities.ScalaProlog._
 
 class ScalaPrologTest extends FunSuite {
-  test("Extract a character from prolog should return a character") {
-    assert(getCharacter("Jacob").isInstanceOf[Character])
+  test("Extract a character from prolog should return the appropriate character") {
+    assert(getCharacter("Jacob").isInstanceOf[Character] &&
+      getCharacter("Jacob").characterName == "Jacob")
   }
-  test("Extract a special move from prolog should return a special move") {
-    assert(getSpecialMove("Antidote").isInstanceOf[SpecialMove])
+  test("Extract a special move from prolog should return the appropriate special move") {
+    assert(getSpecialMove("Antidote").isInstanceOf[SpecialMove] &&
+      getSpecialMove("Antidote").name == "Antidote")
   }
-  test("Extract a modifier from prolog should return a modifier") {
-    assert(getModifier("Concentrated").isInstanceOf[Modifier])
+  test("Extract a modifier from prolog should return the appropriate modifier") {
+    assert(getModifier("Concentrated").isInstanceOf[Modifier] &&
+      getModifier("Concentrated").name == "Concentrated")
   }
 }
