64be047a8836ff7676868fc40e94850912e05ba4
Daniele Schiavi <daniele.schiavi@studio.unibo.it>
2018-12-11 17:09:52 +0100

    Refactor ScalaProlog and ScalaProlog tests

--- shared/src/test/scala/model/ScalaPrologTest.scala
+++ shared/src/test/scala/model/ScalaPrologTest.scala
@@ -4,36 +4,6 @@ import org.scalatest.FunSuite
 import utilities.ScalaProlog._
 
 class ScalaPrologTest extends FunSuite {
-  private val charList: List[String] =
-    List("Jacob", "Annabelle", "Albert", "Lidya", "Cassandra", "Noah", "Fernando", "Nora")
-  private val jacobMoveList: List[String] =
-    List("Physical Attack", "Skullcrack", "Sismic Slam", "Berserker Rage", "Second Wind")
-  private val spellList: List[String] = List("Lay On Hands",
-    "Censure",
-    "Bolster Faith",
-    "Preparation",
-    "Second Wind",
-    "Berserker Rage",
-    "Freezing Wind",
-    "Hypnosis",
-    "Concentration",
-    "Flamestrike")
-
-  import scala.collection.mutable.ListBuffer
-  import alice.tuprolog.NoMoreSolutionException
-  val charactersList: ListBuffer[Character] = ListBuffer()
-  try {
-    getAllCharacters().foreach(v => charactersList += v)
-  } catch {
-    case ex: NoMoreSolutionException => Unit
-  }
-  test("Extract all characters from prolog should return all the characters") {
-    assert(
-      charactersList
-        .map(character => character.characterName)
-        .toList == charList)
-  }
-
   test("Extract a character from prolog should return a character") {
     assert(getCharacter("Jacob").isInstanceOf[Character])
   }
