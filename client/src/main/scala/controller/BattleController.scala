628f3955f06fb87819ceaab320edb78f64c3c209
Nicola Atti <nicola.atti@studio.unibo.it>
2018-12-12 15:31:22 +0100

    Fix act button activation

--- client/src/main/scala/controller/BattleController.scala
+++ client/src/main/scala/controller/BattleController.scala
@@ -217,7 +217,7 @@ object BattleController extends Initializable with ViewController {
   }
 
   private def cannotAct: Boolean = {
-    if (!moveListView.getSelectionModel.getSelectedItems.isEmpty) {
+    if (!moveListView.getSelectionModel.getSelectedItems.isEmpty && targets.nonEmpty) {
       val moveName = moveListView.getSelectionModel.getSelectedItem
       var moveMaxTargets = 1
       moveName match {
