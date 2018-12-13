7fea6b2dd55d4b7a40bec3613c3a5da982383eab
Daniele Schiavi <daniele.schiavi@studio.unibo.it>
2018-12-13 03:45:02 +0100

    Fix matchmaking system for corner cases

--- services/matchmaking/src/main/scala/AsyncDbManager.scala
+++ services/matchmaking/src/main/scala/AsyncDbManager.scala
@@ -1,8 +1,9 @@
 import scala.concurrent.Future
 import org.mongodb.scala._
 import org.mongodb.scala.bson.BsonValue
-import org.mongodb.scala.model.Filters
 import org.mongodb.scala.result.DeleteResult
+import org.mongodb.scala.model.Filters
+import org.mongodb.scala.model.Updates._
 
 object AsyncDbManager {
 
@@ -10,37 +11,44 @@ object AsyncDbManager {
   val database = mongoClient.getDatabase("heroku_3bppsqjk")
   val queueCollection = database.getCollection("casual-queue")
   val battleCollection = database.getCollection("active-battles")
-  val documentId = "casual-queue"
 
-  val filter = Filters.equal("_id", value = documentId)
+  val ticketDocumentId = "casual-queue-ticket"
+  val field = "ticket"
 
-  def findPlayerInQueue: Future[(String, Seq[String], String)] = {
+  def getTicket: Future[Int] = {
     queueCollection
-      .findOneAndDelete(filter)
+      .findOneAndUpdate(filter = Filters.equal("_id", value = ticketDocumentId), update = inc(field, number = 1))
+      .map(oldDocument => oldDocument(field).asInt32().getValue)
+      .head()
+  }
+
+  def takePlayerFromQueue(ticket: Int): Future[(String, Seq[String], String)] = {
+    queueCollection
+      .findOneAndDelete(filter = Filters.equal("_id", value = ticket))
       .map(document =>
         (document("playerName").asString().getValue, document("teamMembers").asArray().toArray.toSeq.collect {
           case str: BsonValue => str.asString().getValue
-        }, document("replyToQueue").asString().getValue))
+        }, document("replyTo").asString().getValue))
       .head()
   }
 
-  def putPlayerInQueue(playerName: String, teamMembers: Seq[String], replyTo: String): Future[Completed] = {
+  def putPlayerInQueue(ticket: Int,
+                       playerName: String,
+                       teamMembers: Seq[String],
+                       replyTo: String): Future[Completed] = {
     queueCollection
       .insertOne(
-        Document("_id" -> documentId,
-                 "playerName" -> playerName,
-                 "teamMembers" -> teamMembers,
-                 "replyToQueue" -> replyTo))
+        Document("_id" -> ticket, "playerName" -> playerName, "teamMembers" -> teamMembers, "replyTo" -> replyTo))
       .head()
   }
 
-  def createBattleInstance(player1Name: String, player2Name: String): Future[Completed] = {
+  def createBattleInstance(player1Name: String, player2Name: String, battleId: String): Future[Completed] = {
     battleCollection
-      .insertOne(Document("player1" -> player1Name, "player2" -> player2Name))
+      .insertOne(Document("_id" -> battleId, "player1" -> player1Name, "player2" -> player2Name))
       .head()
   }
 
-  def removeQueuedPlayer(playerName: String): Future[DeleteResult] = {
+  def removePlayerFromQueue(playerName: String): Future[DeleteResult] = {
     queueCollection.deleteOne(Filters.equal("playerName", playerName)).head()
   }
 
--- services/matchmaking/src/main/scala/Main.scala
+++ services/matchmaking/src/main/scala/Main.scala
@@ -4,7 +4,6 @@ import ExecutionContext.Implicits.global
 import akka.actor.{ActorSystem, Props}
 import com.spingo.op_rabbit._
 import com.spingo.op_rabbit.properties.ReplyTo
-import communication.MessageFormat.MyFormat
 
 object Main extends App {
 
@@ -33,26 +32,36 @@ object Main extends App {
             }
             request.operation match {
               case "Add" =>
-                AsyncDbManager.findPlayerInQueue.onComplete {
-                  case Success(opponentData: (String, Seq[String], String)) =>
-                    val reqPlayerData = (request.playerName, request.team, replyTo.get)
-                    AsyncDbManager.createBattleInstance(request.playerName, opponentData._1).onComplete {
-                      case Success(_) => sendBattleDataToBoth(reqPlayerData, opponentData)
+                AsyncDbManager.getTicket.onComplete {
+                  case Success(ticket: Int) =>
+                    AsyncDbManager.putPlayerInQueue(ticket, request.playerName, request.team, replyTo.get).onComplete {
+                      case Success(_) => {
+                        val opponentTicket = if (ticket % 2 == 0) ticket - 1 else ticket + 1
+                        AsyncDbManager.takePlayerFromQueue(opponentTicket).onComplete {
+                          case Success((opponentName, opponentTeam, opponentReplyTo)) => {
+                            AsyncDbManager.removePlayerFromQueue(request.playerName)
+                            val battleId =
+                              if (ticket % 2 == 0) opponentTicket + "-" + ticket
+                              else ticket + "-" + opponentTicket
+                            AsyncDbManager.createBattleInstance(request.playerName, opponentName, battleId).onComplete {
+                              case Success(_) => {
+                                sendBattleDataToBoth((request.playerName, request.team, replyTo.get),
+                                                     (opponentName, opponentTeam, opponentReplyTo),
+                                                     battleId)
+                              }
+                              case Failure(e) => println(s"Failure... Caught: $e")
+                            }
+                          }
+                          case Failure(e) => println(s"Failure... Caught: $e")
+                          case _          => println(s"Could not find opponent with ticket $opponentTicket")
+                        }
+                      }
                       case Failure(e) => println(s"Failure... Caught: $e")
                     }
-
-                  case Failure(e) =>
-                    println(s"Failure... Caught: $e")
-                  case _ =>
-                    AsyncDbManager
-                      .putPlayerInQueue(request.playerName.toString, request.team, replyTo.get.toString)
-                      .onComplete {
-                        case Success(_)         => println("Database updated successfully")
-                        case Failure(exception) => println(s"Failure... Caught: $exception")
-                      }
+                  case Failure(e) => println(s"Failure... Caught: $e")
                 }
               case "Remove" =>
-                AsyncDbManager.removeQueuedPlayer(request.playerName).onComplete {
+                AsyncDbManager.removePlayerFromQueue(request.playerName).onComplete {
                   case Success(_)         => println("Client removed successfully")
                   case Failure(exception) => println(s"Failure... Caught: $exception")
                 }
@@ -65,9 +74,11 @@ object Main extends App {
   }
 
   def sendBattleDataToBoth(dataReqPlayer: (String, Seq[String], String),
-                           dataQueuedPlayer: (String, Seq[String], String)): Unit = {
-    val responseForRequester = JoinCasualQueueResponse(Right((dataQueuedPlayer._1, dataQueuedPlayer._2)))
-    val responseForQueued = JoinCasualQueueResponse(Right((dataReqPlayer._1, dataReqPlayer._2)))
+                           dataQueuedPlayer: (String, Seq[String], String),
+                           battleId: String): Unit = {
+    val responseForRequester = JoinCasualQueueResponse(
+      Right((dataQueuedPlayer._1, dataQueuedPlayer._2.toList, battleId)))
+    val responseForQueued = JoinCasualQueueResponse(Right((dataReqPlayer._1, dataReqPlayer._2.toList, battleId)))
     rabbitControl ! Message.queue(responseForRequester, dataReqPlayer._3)
     rabbitControl ! Message.queue(responseForQueued, dataQueuedPlayer._3)
   }
