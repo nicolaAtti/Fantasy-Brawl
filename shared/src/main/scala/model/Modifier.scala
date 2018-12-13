1c1e064c3ea6d350f8c08efa0e4a3497431707b7
Marco Canducci <marco.candoz@gmail.com>
2018-12-13 21:26:39 +0100

    Change doc style from Java-like to Scala-like

--- shared/src/main/scala/model/Modifier.scala
+++ shared/src/main/scala/model/Modifier.scala
@@ -1,10 +1,9 @@
 package model
 
-/**
-  * Contains all the elements that define a modifier
+/** Contains all the elements that define a modifier.
   *
-  * @param name the name of the modifier
-  * @param affectsSubStatistic the SubStatistic affected by the modifier
+  * @param name the unique name of the modifier
+  * @param affectsSubStatistic the sub-statistic affected by the modifier
   * @param delta the value to add/subtract
   * @param roundsDuration the number of rounds when the modifier is in effect
   */
