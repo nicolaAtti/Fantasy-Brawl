ac94b924f76b1b22fc14aff5fddd5076219cf26d
Marco Canducci <marco.candoz@gmail.com>
2018-12-14 12:31:27 +0100

    Update characters statistics

--- shared/src/main/resources/model/PrologCharacters.pl
+++ shared/src/main/resources/model/PrologCharacters.pl
@@ -1,26 +1,22 @@
-
 % character contains all the informations in order to create a new character in the game
 % character(@Name,@Class,@Strength,@Agility,@Spirit,@Intelligence,@Resistence,@MoveList)
 %
 % Author: Nicola Atti
 
-
-character('Jacob','Warrior',51,33,13,5,27,['Skullcrack','Sismic Slam','Berserker Rage','Second Wind']).
-character('Annabelle','Thief',34,46,22,8,27,['Poison Vial','Flash Grenade','Preparation','Backstab']).
-character('Albert','Healer',19,11,33,54,27,['Lay On Hands','Holy Smite','Censure','Bolster Faith']).
+character('Jacob','Warrior',51,41,13,5,27,['Skullcrack','Sismic Slam','Berserker Rage','Second Wind']).
+character('Annabelle','Thief',39,40,22,8,27,['Poison Vial','Flash Grenade','Preparation','Backstab']).
+character('Albert','Healer',10,25,33,54,27,['Lay On Hands','Holy Smite','Censure','Bolster Faith']).
 character('Lidya','Wizard',7,22,34,58,23,['Ice Lance','Counterspell','Freezing Wind','Concentration']).
 
-character('Cassandra','Warrior',33,20,32,11,37,['Shield Bash','Second Wind','Trip Attack','Sunder Defenses']).
+character('Cassandra','Warrior',33,22,32,11,37,['Shield Bash','Second Wind','Trip Attack','Sunder Defenses']).
 character('Noah','Warrior',70,10,13,5,35,['Adrenaline Rush','Skullcrack','Iron Will','Mighty Slam']).
-character('Aster','Thief',41,33,30,8,28,['Trueshot','Arrow Volley','Aimed Shot','Bear Trap']).
-character('Rikh','Wizard',4,17,40,49,29,['Chrono Shift','Haste','Hypnosis','Arcane Blast']).
+character('Aster','Thief',41,28,30,8,28,['Trueshot','Arrow Volley','Aimed Shot','Bear Trap']).
+character('Rikh','Wizard',4,30,40,51,29,['Chrono Shift','Haste','Hypnosis','Arcane Blast']).
 
 character('Fernando','Thief',46,29,26,6,34,['Quick Stab','Fan Of Knives','Duelist Step','Pummel']).
-character('Nora','Healer',7,50,28,49,26,['Benefic Spores','Nature Wrath','Antidote','Entangling Roots']).
-character('Norman','Healer',12,25,38,47,22,['Lay On Hands','Holy Nova','Radiance','Chastise']).
+character('Nora','Healer',3,62,28,49,26,['Benefic Spores','Nature Wrath','Antidote','Entangling Roots']).
+character('Norman','Healer',3,44,38,47,22,['Lay On Hands','Holy Nova','Radiance','Chastise']).
 character('Linn','Wizard',9,25,30,60,25,['Flamestrike','Dragon Breath','Pyroblast','Concentration']).
 
 
-
-
-% Other characters will be added in the future
+% Other characters can be added in the future
