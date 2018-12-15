dfb5ebe7cf265a02077248cbe274e9be3e88f48c
Marco Canducci <marco.candoz@gmail.com>
2018-12-15 17:29:41 +0100

    Start turn ordering database handling

--- services/turn-ordering/src/main/scala/turnordering/MongoDbManager.scala
+++ services/turn-ordering/src/main/scala/turnordering/MongoDbManager.scala
@@ -1,3 +1,23 @@
 package turnordering
 
-object MongoDbManager {}
+import org.mongodb.scala.result.DeleteResult
+import org.mongodb.scala.{Completed, MongoClient, MongoDatabase}
+
+import scala.concurrent.Future
+
+object MongoDbManager {
+
+  import config._
+  val database: MongoDatabase = MongoClient(DbSettings.MongoClientStr).getDatabase(DbSettings.MongoDatabaseStr)
+
+  def getCurrentRound(battleId: String): Future[Int] = ???
+
+  def incrementCurrentRound(battleId: String): Future[Completed] = ???
+
+  def addPlayerInfo(playerInfo: PlayerInfo): Future[Completed] = ???
+
+  def getPlayerInfo(playerName: String, battleId: String): Future[PlayerInfo] = ???
+
+  def deletePlayerInfo(playerName: String, battleId: String): Future[DeleteResult] = ???
+
+}
