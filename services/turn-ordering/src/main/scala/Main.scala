6f00eb4bea7201dcbe59ede1debad4203c31f222
Marco Canducci <marco.candoz@gmail.com>
2018-12-15 17:48:55 +0100

    Remove old code from turn ordering service and scalafmt style minor refactor

--- services/turn-ordering/src/main/scala/Main.scala
+++ services/turn-ordering/src/main/scala/Main.scala
@@ -6,9 +6,6 @@ import com.spingo.op_rabbit.properties.ReplyTo
 import communication.MessageFormat.MyFormat
 import config.MessagingSettings
 
-/** Entry point of the service that handles the login for guests users.
-  * @author Marco Canducci
-  */
 object Main extends App {
 
   import communication._
@@ -21,8 +18,6 @@ object Main extends App {
 
   import Queues._
 
-  var boh: Option[(Map[(String, String), Int], Int, String)] = None
-
   val requestQueue =
     Queue(StartRoundRequestQueue, durable = MessagingSettings.Durable, autoDelete = MessagingSettings.AutoDelete)
   Subscription.run(rabbitControl) {
@@ -31,26 +26,7 @@ object Main extends App {
       consume(requestQueue) {
         (body(as[StartRoundRequest]) & optionalProperty(ReplyTo)) { (request, replyTo) =>
           {
-            if (boh.isEmpty) {
-              boh = Some(request.myTeamSpeeds.map {
-                case (characterName, speed) => (request.playerName, characterName) -> speed
-              }, request.round, replyTo.get)
-            } else {
-              println(request.myTeamSpeeds.map {
-                case (characterName, speed) => (request.playerName, characterName) -> speed
-              })
-              println(boh.get._1)
-              val speedsNotOrdered = (request.myTeamSpeeds.map {
-                case (characterName, speed) => (request.playerName, characterName) -> speed
-              } ++ boh.get._1).toList
-              val speedsOrdered: List[(String, String)] =
-                speedsNotOrdered.sortBy(_._2).reverse.map(c => c._1._1 -> c._1._2)
-              println(speedsOrdered)
-              val response = StartRoundResponse(Right(speedsOrdered), request.round)
-              rabbitControl ! Message.queue(response, replyTo.get)
-              rabbitControl ! Message.queue(response, boh.get._3)
-              boh = None
-            }
+
             ack
           }
         }
