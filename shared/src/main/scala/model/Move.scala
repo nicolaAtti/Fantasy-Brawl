434a5e2b19cfee37b1b77176b5d503fea2af90c3
Marco Canducci <marco.candoz@gmail.com>
2018-12-15 02:24:45 +0100

    Move refactoring and extensive documentation

--- shared/src/main/scala/model/Move.scala
+++ shared/src/main/scala/model/Move.scala
@@ -5,6 +5,10 @@ import MoveType._
 import MoveEffectStrategies._
 import model.Alteration._
 
+/** A move is described by its type, the amount of mana points that it costs, the
+  * maximum targets that it can be applied to, and its move effect: the move effect
+  * will provide the new status of the target character hit by the move.
+  */
 sealed trait Move {
   def moveType: MoveType
   def moveEffect: (Attacker, Target) => NewTargetStatus
@@ -20,39 +24,87 @@ object Move {
   type NewTargetStatus = Status
   type NewStatuses = Map[Character, Status]
 
+  /** Tells if a character can or cannot make a move.
+    *
+    * @param attacker the character that wants to make a move
+    * @param move the move
+    * @return true if the attacker can make the move, false otherwise
+    */
   def canMakeMove(attacker: Attacker, move: Move): Boolean =
     hasEnoughMana(attacker, move) && !isInhibited(attacker, move)
 
+  /** Provides the new status of every character involved in the move execution.
+    *
+    * A move will affect all the targets (evaluating the move effect for everyone
+    * of them) and possibly the attacker if the move costs some mana points.
+    * It's worth mentioning that a character can be both attacker and target at
+    * the same time: useful for example if a he/she wants to cure himself/herself
+    * casting a healing spell.
+    *
+    * @param move the move to be executed
+    * @param attacker the character that makes the move
+    * @param targets the character/s that undergo the move effects
+    * @return the new status for each character involved
+    */
   def makeMove(move: Move, attacker: Attacker, targets: Set[Target]): NewStatuses = {
-    require(canMakeMove(attacker, move) && targets.size <= move.maxTargets)
-
     val results: NewStatuses =
       targets
-        .map(target => (target, move.moveEffect(attacker, target)))
+        .map(target => target -> move.moveEffect(attacker, target))
         .toMap
 
     import Status._
-    if (results contains attacker)
+    if (move.manaCost > 0 && results.contains(attacker))
       adjust(map = results, key = attacker)(afterManaConsumption(_, move))
-    else
+    else if (move.manaCost > 0)
       results + (attacker -> afterManaConsumption(attacker.status, move))
+    else
+      results
   }
 
   private object MoveHelper {
 
+    /** Denotes if a character currently has enough mana to execute a move.
+      *
+      * @param character the character that wants to make a move
+      * @param move the move
+      * @return true if the character has enough mana, false otherwise
+      */
     def hasEnoughMana(character: Attacker, move: Move): Boolean = character.status.manaPoints >= move.manaCost
 
+    /** Denotes if a character is currently affected by an alteration that
+      * inhibits a certain move.
+      *
+      * @param character the character that wants to make a move
+      * @param move the move
+      * @return true if the character is inhibited, false otherwise
+      */
     def isInhibited(character: Attacker, move: Move): Boolean =
       character.status.alterations
         .map { case (alteration, _) => alteration.inhibits(move) }
         .fold(false)(_ || _)
 
+    /** Evaluates a new Map, given: a Map, a key contained inside that map, and
+      * a function. The new map will contain the updated value for the given key,
+      * obtained applying the given function to the corresponding old value.
+      *
+      * @param map the old map
+      * @param key a key value contained inside the map
+      * @param f the transformation function
+      * @tparam A the key type
+      * @tparam B the value type
+      * @return the new, updated map
+      */
     def adjust[A, B](map: Map[A, B], key: A)(f: B => B): Map[A, B] = map.updated(key, f(map(key)))
 
   }
 
 }
 
+/** The basic move, practicable by every character.
+  *
+  * It is of type Melee, it doesn't cost any mana points and can hit only one
+  * target at a time. Its move effect is a standard damage with zero base damage.
+  */
 case object PhysicalAttack extends Move {
   override val moveType: MoveType = Melee
   override val moveEffect: (Attacker, Target) => NewTargetStatus =
@@ -63,6 +115,14 @@ case object PhysicalAttack extends Move {
   override val maxTargets = 1
 }
 
+/** A special move.
+  *
+  * @param name the move name
+  * @param moveType the move type
+  * @param moveEffect the move effect
+  * @param manaCost how many mana points the attacker must spend to make the move
+  * @param maxTargets how many targets can be hit at once
+  */
 case class SpecialMove(name: String,
                        moveType: MoveType,
                        moveEffect: (Attacker, Target) => NewTargetStatus,
@@ -75,6 +135,23 @@ case class SpecialMove(name: String,
 
 object SpecialMove {
 
+  /** Provides the appropriate SpecialMove object given its name, its move effect
+    * string code, its move type, all the modifiers to add and the alterations to
+    * add/remove, the mana points that it costs and the maximum number of targets
+    * that can be hit at once.
+    *
+    * @param name the move name
+    * @param moveEffectStrategyCode the move effect string code
+    * @param moveType the move type
+    * @param baseValue the base value of the move effect
+    * @param addModifiers the modifiers that the move adds to the targets
+    * @param addAlterations the alterations that the move adds to the targets
+    * @param removeAlterations the alterations to remove from the targets
+    * @param manaCost the mana points that the attacker character must spend in
+    *                 order to make the move
+    * @param maxTargets the maximum number of characters that can be targeted at once
+    * @return the SpecialMove object
+    */
   def apply(name: String,
             moveEffectStrategyCode: String,
             moveType: MoveType,
--- shared/src/main/scala/model/MoveEffectStrategies.scala
+++ shared/src/main/scala/model/MoveEffectStrategies.scala
@@ -2,6 +2,7 @@ package model
 
 import scala.util.Random
 
+/** A factory of the possible move effects present in the game. */
 object MoveEffectStrategies {
 
   val minDamage = 1
@@ -9,6 +10,20 @@ object MoveEffectStrategies {
   import model.Move._
   import StrategiesHelper._
 
+  /** Provides a standard damage effect that describes how to:
+    * 1) Decrease the target's health points.
+    * 2) Possibly affect the modifiers and the alterations of the target's status.
+    *
+    * @param moveType the move type
+    * @param baseDamage the base damage value
+    * @param addModifiers the modifiers to add (or to update if already present)
+    *                     to the target's status
+    * @param addAlterations the alterations to add (or to update if already present)
+    *                       to the target's status
+    * @param removeAlterations the alterations to remove (if present) from the
+    *                          target's status
+    * @return the move effect modeling an appropriate standard damage
+    */
   def createStandardDamageEffect(moveType: MoveType, baseDamage: Int)(implicit addModifiers: Map[String, Modifier],
                                                                       addAlterations: Map[Alteration, Int],
                                                                       removeAlterations: Set[Alteration]) =
@@ -22,6 +37,19 @@ object MoveEffectStrategies {
       updatedStatus(originalStatus = target.status, healthPointsToClamp = target.status.healthPoints - actualDamage)
     }
 
+  /** Provides a standard heal effect that describes how to:
+    * 1) Increase the target's health points.
+    * 2) Possibly affect the modifiers and the alterations of the target's status.
+    *
+    * @param baseHeal the base heal value
+    * @param addModifiers the modifiers to add (or to update if already present)
+    *                     to the target's status
+    * @param addAlterations the alterations to add (or to update if already present)
+    *                       to the target's status
+    * @param removeAlterations the alterations to remove (if present) from the
+    *                          target's status
+    * @return the move effect modeling an appropriate standard healing
+    */
   def createStandardHealEffect(baseHeal: Int)(implicit addModifiers: Map[String, Modifier],
                                               addAlterations: Map[Alteration, Int],
                                               removeAlterations: Set[Alteration]) =
@@ -35,12 +63,38 @@ object MoveEffectStrategies {
       updatedStatus(originalStatus = target.status, healthPointsToClamp = target.status.healthPoints + actualHeal)
     }
 
+  /** Provides a percentage move effect that describes how to:
+    * 1) Increase/decrease the target's health points by a certain percentage.
+    * 2) Possibly affect the modifiers and the alterations of the target's status.
+    *
+    * @param percentage the percentage value that will be multiplied by the health
+    *                   points of the target (formula: hp * percentage / 100)
+    * @param addModifiers the modifiers to add (or to update if already present)
+    *                     to the target's status
+    * @param addAlterations the alterations to add (or to update if already present)
+    *                       to the target's status
+    * @param removeAlterations the alterations to remove (if present) from the
+    *                          target's status
+    * @return the move effect modeling an appropriate percentage modification
+    */
   def createPercentageEffect(percentage: Int)(implicit addModifiers: Map[String, Modifier],
                                               addAlterations: Map[Alteration, Int],
                                               removeAlterations: Set[Alteration]) =
     (_: Attacker, target: Target) =>
       updatedStatus(originalStatus = target.status, healthPointsToClamp = target.status.healthPoints * percentage / 100)
 
+
+  /** Provides a buff/debuff move effect: this type of effect only affects the
+    * modifiers and the alterations of the target's status.
+    *
+    * @param addModifiers the modifiers to add (or to update if already present)
+    *                     to the target's status
+    * @param addAlterations the alterations to add (or to update if already present)
+    *                       to the target's status
+    * @param removeAlterations the alterations to remove (if present) from the
+    *                          target's status
+    * @return the move effect modeling an appropriate buff/debuff modification
+    */
   def createBuffDebuffEffect(implicit addModifiers: Map[String, Modifier],
                              addAlterations: Map[Alteration, Int],
                              removeAlterations: Set[Alteration]) =
@@ -49,6 +103,17 @@ object MoveEffectStrategies {
 
   private object StrategiesHelper {
 
+    /** Determines the actual value, starting from the base value and evaluating
+      * if the critical bonus must be applied or not.
+      *
+      * @param value the base value (base damage or base heal)
+      * @param criticalChance the probability to strike a critical move
+      * @param criticalBonus the multiplicative factor to apply to the base value
+      *                      in case the move is evaluated as critical
+      * @return the actual value (equal to the base value if the move is evaluated
+      *         as not critical, equal to base value multiplied to the critical
+      *         bonus otherwise)
+      */
     def afterCriticalEvaluation(value: Int, criticalChance: Int, criticalBonus: Double): Int = {
       val criticalValue = value * criticalBonus / 100
       val isCritical = criticalChance > Random.nextInt(100)
@@ -61,14 +126,16 @@ object MoveEffectStrategies {
       * the new health points (to validate) plus all the modifiers to add and the
       * alterations to add/remove to/from the original status.
       *
-      * @param originalStatus the status before the move effect
+      * @param originalStatus the original status before the move effect
       * @param healthPointsToClamp the new health points to clamp between zero and
-      *                            the maximum health points
-      * @param modifiersToAdd the modifiers to add to the new status
-      * @param alterationsToAdd the alterations to add to the new status
+      *                            the maximum possible health points
+      * @param modifiersToAdd the modifiers to add (or to update if already present)
+      *                       to the original status
+      * @param alterationsToAdd the alterations to add (or to update if already
+      *                         present) to the original status
       * @param alterationsToRemove the alterations to remove (if present) from the
       *                            original status
-      * @return
+      * @return the updated status
       */
     def updatedStatus(originalStatus: Status, healthPointsToClamp: Int)(
         implicit modifiersToAdd: Map[String, Modifier],
