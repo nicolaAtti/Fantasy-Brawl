361dc775dfcc956c6fa22038e3cfd43403e9e45d
Marco Canducci <marco.candoz@gmail.com>
2018-12-16 14:55:54 +0100

    Add round to PlayerInfo

--- services/turn-ordering/src/main/scala/turnordering/PlayerInfo.scala
+++ services/turn-ordering/src/main/scala/turnordering/PlayerInfo.scala
@@ -1,3 +1,3 @@
 package turnordering
 
-case class PlayerInfo(name: String, teamSpeeds: Map[String, Int], battleId: String, replyTo: String)
+case class PlayerInfo(name: String, teamSpeeds: Map[String, Int], battleId: String, round: Int, replyTo: String)
