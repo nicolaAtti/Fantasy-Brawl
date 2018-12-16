b1d1ff05b11731f3e238378318d810d92d5e74b0
Marco Canducci <marco.candoz@gmail.com>
2018-12-16 12:42:03 +0100

    Implement incrementCurrentRound in turn ordering dbmanager

--- services/turn-ordering/src/main/scala/turnordering/MongoDbManager.scala
+++ services/turn-ordering/src/main/scala/turnordering/MongoDbManager.scala
@@ -1,8 +1,10 @@
 package turnordering
 
 import org.mongodb.scala.bson.collection.immutable.Document
+import org.mongodb.scala.bson.conversions.Bson
 import org.mongodb.scala.model.Filters
-import org.mongodb.scala.result.DeleteResult
+import org.mongodb.scala.model.Updates.inc
+import org.mongodb.scala.result.{DeleteResult, UpdateResult}
 import org.mongodb.scala.{Completed, MongoClient, MongoCollection, MongoDatabase}
 
 import scala.util.{Failure, Success}
@@ -23,12 +25,23 @@ object MongoDbManager {
     */
   def getCurrentRound(battleId: String): Future[Int] = {
     battlesCollection
-      .find(filter = Filters.equal(fieldName = "_id", value = battleId))
+      .find(filter = Filters.equal("_id", battleId))
       .map(document => document(ActiveBattles.CurrentRound).asInt32().getValue)
       .head() // returns the Observable's head in a Future
   }
 
-  def incrementCurrentRound(battleId: String): Future[Completed] = ???
+  /** Increments the current round of a given active battle.
+    *
+    * @param battleId the battle identifier
+    * @return a Future representing the success/failure of the operation
+    */
+  def incrementCurrentRound(battleId: String): Future[UpdateResult] = {
+    val incByOne: Bson = inc(ActiveBattles.CurrentRound, number = 1)
+
+    battlesCollection
+      .updateOne(filter = Filters.equal("_id", battleId), update = incByOne)
+      .head()
+  }
 
   def addPlayerInfo(playerInfo: PlayerInfo): Future[Completed] = ???
 
@@ -41,11 +54,26 @@ object MongoDbManager {
 object ManagerTesting extends App {
   import MongoDbManager._
 
+
   getCurrentRound(battleId = "1-2").onComplete {
-    case Success(round) => println("Success: "+round)
+    case Success(round) => println("Success: " + round)
     case Failure(e)     => println(s"Failure: $e")
   }
 
-  Thread.sleep(4000)
+  Thread.sleep(3000)
+
+  incrementCurrentRound(battleId = "1-2").onComplete{
+    case Success(success) => println("Success: " + success)
+    case Failure(e)     => println(s"Failure: $e")
+  }
+
+  Thread.sleep(3000)
+
+  getCurrentRound(battleId = "1-2").onComplete {
+    case Success(round) => println("Success: " + round)
+    case Failure(e)     => println(s"Failure: $e")
+  }
+
+  Thread.sleep(3000)
 
 }
