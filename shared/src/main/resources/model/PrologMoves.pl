cc3bf16533eabfc9d8893c581ac22e7185d53117
Nicola Atti <nicola.atti@studio.unibo.it>
2018-12-17 23:37:44 +0100

    Move balancing

--- shared/src/main/resources/model/PrologMoves.pl
+++ shared/src/main/resources/model/PrologMoves.pl
@@ -3,52 +3,52 @@
 %
 % Author: Nicola Atti
 
-spec_move('Lay On Hands','StandardHeal','Spell',50,[],[],[],15,1).
-spec_move('Censure','BuffDebuff','Spell',0,[],['Sil'],[],30,1).
-spec_move('Holy Smite','StandardDamage','Melee',30,[],[],['Slp'],25,1).
+spec_move('Lay On Hands','StandardHeal','Spell',60,[],[],[],20,1).
+spec_move('Censure','BuffDebuff','Spell',0,[],['Sil'],[],15,1).
+spec_move('Holy Smite','StandardDamage','Melee',15,[],[],['Slp'],10,1).
 spec_move('Bolster Faith','BuffDebuff','Spell',0,['Bolstered Faith'],[],[],40,4).
-spec_move('Poison Vial','StandardDamage','Ranged',20,[],['Psn'],['Slp'],15,1).
-spec_move('Flash Grenade','BuffDebuff','Ranged',0,[],['Bln'],['Slp'],20,4).
-spec_move('Backstab','StandardDamage','Melee',60,[],[],['Slp'],15,1).
-spec_move('Preparation','BuffDebuff','Spell',0,['Prepared'],[],[],20,1).
-spec_move('Second Wind','Percentage','Spell',175,[],[],[],20,1).
-spec_move('Sismic Slam','StandardDamage','Melee',40,[],[],['Slp'],15,4).
-spec_move('Skullcrack','StandardDamage','Melee',30,[],['Stn'],['Slp'],20,1).
-spec_move('Berserker Rage','BuffDebuff','Spell',0,[],['Brk'],[],30,1).
-spec_move('Freezing Wind','StandardDamage','Spell',25,[],['Frz'],['Slp'],25,4).
-spec_move('Ice Lance','StandardDamage','Spell',40,[],[],['Slp'],20,1).
+spec_move('Poison Vial','StandardDamage','Ranged',15,[],['Psn'],['Slp'],20,1).
+spec_move('Flash Grenade','BuffDebuff','Ranged',0,[],['Bln'],['Slp'],25,4).
+spec_move('Backstab','StandardDamage','Melee',75,[],[],['Slp'],20,1).
+spec_move('Preparation','BuffDebuff','Spell',0,['Prepared'],[],[],15,1).
+spec_move('Second Wind','Percentage','Spell',175,[],[],[],25,1).
+spec_move('Sismic Slam','StandardDamage','Melee',20,[],[],['Slp'],40,4).
+spec_move('Skullcrack','StandardDamage','Melee',20,[],['Stn'],['Slp'],20,1).
+spec_move('Berserker Rage','BuffDebuff','Spell',0,[],['Brk'],[],25,1).
+spec_move('Freezing Wind','StandardDamage','Spell',20,[],['Frz'],['Slp'],35,4).
+spec_move('Ice Lance','StandardDamage','Spell',60,[],[],['Slp'],20,1).
 spec_move('Concentration','BuffDebuff','Spell',0,['Concentrated'],[],[],15,1).
-spec_move('Counterspell','BuffDebuff','Spell',0,[],['Sil'],['Slp'],15,1).
-spec_move('Shield Bash','StandardDamage','Melee',10,[],['Sil'],['Slp'],20,1).
-spec_move('Adrenaline Rush','BuffDebuff','Spell',0,['Sped Up'],[],[],15,1).
-spec_move('Iron Will','BuffDebuff','Spell',0,['Will Of Iron'],[],[],15,1).
-spec_move('Mighty Slam','StandardDamage','Melee',40,[],[],['Slp'],25,1).
-spec_move('Trip Attack','StandardDamage','Melee',5,['Slowed'],[],['Slp'],15,1).
-spec_move('Sunder Defenses','StandardDamage','Melee',5,['Sundered Defences'],[],['Slp'],15,1).
-spec_move('Chrono Shift','Percentage','Spell',85,[],[],['Slp'],30,4).
-spec_move('Haste','BuffDebuff','Spell',0,['Sped Up'],[],[],20,1).
+spec_move('Counterspell','BuffDebuff','Spell',0,[],['Sil'],[],20,1).
+spec_move('Shield Bash','StandardDamage','Melee',10,[],['Sil'],['Slp'],25,1).
+spec_move('Adrenaline Rush','BuffDebuff','Spell',0,['Sped Up'],[],[],20,1).
+spec_move('Iron Will','BuffDebuff','Spell',0,['Will Of Iron'],[],[],20,1).
+spec_move('Mighty Slam','StandardDamage','Melee',40,[],[],['Slp'],20,1).
+spec_move('Trip Attack','StandardDamage','Ranged',10,['Slowed'],[],['Slp'],10,1).
+spec_move('Sunder Defences','StandardDamage','Melee',15,['Sundered Defences'],[],['Slp'],15,1).
+spec_move('Chrono Shift','Percentage','Spell',75,[],[],['Slp'],50,4).
+spec_move('Haste','BuffDebuff','Spell',0,['Sped Up'],[],[],25,1).
 spec_move('Hypnosis','BuffDebuff','Spell',0,[],['Slp'],[],30,1).
-spec_move('Arcane Blast','StandardDamage','Spell',50,[],[],['Slp'],25,1).
-spec_move('Pyroblast','StandardDamage','Spell',35,[],[],['Slp'],15,1).
-spec_move('Dragon Breath','StandardDamage','Spell',15,[],['Stn'],['Slp'],20,1).
-spec_move('Flamestrike','StandardDamage','Spell',40,[],[],['Slp'],25,4).
-spec_move('Holy Nova','StandardDamage','Spell',30,[],[],['Slp'],20,4).
-spec_move('Radiance','Percentage','Spell',140,[],[],[],40,4).
+spec_move('Arcane Blast','StandardDamage','Spell',70,[],[],['Slp'],25,1).
+spec_move('Pyroblast','StandardDamage','Spell',50,[],[],['Slp'],15,1).
+spec_move('Dragon Breath','StandardDamage','Spell',15,[],['Stn'],['Slp'],25,1).
+spec_move('Flamestrike','StandardDamage','Spell',40,[],[],['Slp'],50,4).
+spec_move('Holy Nova','StandardDamage','Spell',20,[],[],['Slp'],25,4).
+spec_move('Radiance','Percentage','Spell',135,[],[],[],40,4).
 spec_move('Chastise','BuffDebuff','Spell',0,[],['Stn'],['Slp'],20,1).
 
-spec_move('Aimed Shot','StandardDamage','Ranged',35,[],[],['Slp'],15,1).
-spec_move('Arrow Volley','StandardDamage','Ranged',20,[],[],['Slp'],25,4).
+spec_move('Aimed Shot','StandardDamage','Ranged',35,[],[],['Slp'],20,1).
+spec_move('Arrow Volley','StandardDamage','Ranged',20,[],[],['Slp'],40,4).
 spec_move('Trueshot','BuffDebuff','Spell',0,['Trueshot'],[],[],20,1).
-spec_move('Bear Trap','BuffDebuff','Spell',10,[],['Frz'],['Slp'],15,1).
+spec_move('Bear Trap','BuffDebuff','Spell',0,[],['Frz'],['Slp'],15,1).
 
 spec_move('Nature Wrath','StandardDamage','Spell',30,[],[],['Slp'],15,1).
-spec_move('Benefic Spores','BuffDebuff','Spell',0,[],['Reg'],[],25,4).
-spec_move('Antidote','BuffDebuff','Spell',0,[],[],['Psn'],20,1).
+spec_move('Benefic Spores','StandardHeal','Spell',35,[],['Reg'],[],40,4).
+spec_move('Antidote','BuffDebuff','Spell',0,[],[],['Psn'],15,1).
 spec_move('Entangling Roots','BuffDebuff','Spell',0,[],['Frz'],[],15,1).
-spec_move('Quick Stab','StandardDamage','Melee',30,[],[],['Slp'],15,1).
-spec_move('Fan Of Knives','StandardDamage','Ranged',25,[],[],['Slp'],10,4).
-spec_move('Duelist Step','BuffDebuff','Spell',0,['Quick Reflexes','Sped Up'],[],[],20,1).
-spec_move('Pummel','StandardDamage','Melee',10,[],['Sil'],['Slp'],15,1).
+spec_move('Quick Stab','StandardDamage','Melee',25,[],[],['Slp'],15,1).
+spec_move('Fan Of Knives','StandardDamage','Ranged',20,[],[],['Slp'],30,4).
+spec_move('Duelist Step','BuffDebuff','Spell',0,['Quick Reflexes','Sped Up'],[],[],30,1).
+spec_move('Pummel','StandardDamage','Melee',15,[],['Sil'],['Slp'],15,1).
 
 
 %modifier --- Defines the duration, the power and the interested attribute for every existing modifier
