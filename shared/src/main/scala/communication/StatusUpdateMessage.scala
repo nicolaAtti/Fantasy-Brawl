1981638f6abc18090494f2e7863a22e05eaa19fc
Daniele Schiavi <daniele.schiavi@studio.unibo.it>
2018-12-17 17:22:35 +0100

    Change from character to attacker

--- shared/src/main/scala/communication/StatusUpdateMessage.scala
+++ shared/src/main/scala/communication/StatusUpdateMessage.scala
@@ -2,7 +2,7 @@ package communication
 
 import communication.StatusUpdateMessage.CharacterKey
 
-case class StatusUpdateMessage(character: CharacterKey, moveName: String, targets: Set[CharacterKey], round: Int)
+case class StatusUpdateMessage(attacker: CharacterKey, moveName: String, targets: Set[CharacterKey], round: Int)
 
 object StatusUpdateMessage{
   type PlayerName = String
