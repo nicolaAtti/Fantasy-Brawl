817bcdf2350ef9e455a85349792b3c145d9a6ef9
Daniele Schiavi <daniele.schiavi@studio.unibo.it>
2018-12-18 03:43:20 +0100

    Change from Character to CharacterKey in newStatuses and change implementation of actCalculation and updateTeamsStatuses

--- shared/src/main/scala/communication/StatusUpdateMessage.scala
+++ shared/src/main/scala/communication/StatusUpdateMessage.scala
@@ -1,8 +1,13 @@
 package communication
 
 import communication.StatusUpdateMessage.CharacterKey
+import model.Status
 
-case class StatusUpdateMessage(attacker: CharacterKey, moveName: String, targets: Set[CharacterKey], round: Int)
+case class StatusUpdateMessage(attacker: CharacterKey,
+                               moveName: String,
+                               targets: Set[CharacterKey],
+                               newStatuses: Map[CharacterKey, Status],
+                               round: Int)
 
 object StatusUpdateMessage {
   type PlayerName = String
