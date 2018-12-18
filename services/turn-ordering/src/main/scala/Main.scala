6dc3aaca0cc299b98ad3f02d0760b663a394c617
Marco Canducci <marco.candoz@gmail.com>
2018-12-18 18:37:20 +0100

    Further refactor and documentation

--- services/turn-ordering/src/main/scala/Main.scala
+++ services/turn-ordering/src/main/scala/Main.scala
@@ -5,11 +5,21 @@ import com.spingo.op_rabbit._
 import com.spingo.op_rabbit.properties.ReplyTo
 import communication.MessageFormat.MyFormat
 import communication.StatusUpdateMessage.CharacterKey
+import communication.turnordering.PlayerInfo
+import turnordering.MongoDbManager._
 import config.MessagingSettings
-import turnordering.PlayerInfo
 
 import scala.util.{Failure, Success}
 
+/** Entry point for the service that provides turn-ordering functionality at the
+  * beginning of every round.
+  *
+  * At every new round, before starting, each player sends the information about
+  * his/hers team members' speeds and awaits for a response from the service
+  * containing all the ordered turns.
+  *
+  * @author Daniele Schiavi
+  */
 object Main extends App {
 
   final val LogMessage = "Received a new turn ordering request"
@@ -28,10 +38,10 @@ object Main extends App {
 
   val requestQueue =
     Queue(StartRoundRequestQueue, durable = MessagingSettings.Durable, autoDelete = MessagingSettings.AutoDelete)
+
   Subscription.run(rabbitControl) {
 
     import com.spingo.op_rabbit.Directives._
-    import turnordering.MongoDbManager._
 
     channel(qos = MessagingSettings.Qos) {
       consume(requestQueue) {
@@ -44,44 +54,16 @@ object Main extends App {
             getCurrentRound(request.battleId).onComplete {
 
               case Success(round: Int) if request.round == round + 1 =>
-                val player =
+                val requestInfo =
                   PlayerInfo(request.playerName, request.playerTeamSpeeds, request.battleId, request.round, replyTo.get)
-                addPlayerInfo(player).onComplete {
+                addPlayerInfo(requestInfo).onComplete {
 
                   case Success(_) =>
                     getPlayerInfo(request.opponentName, request.battleId, request.round).onComplete {
 
-                      case Success(opponent) if opponent != null =>
-                        val firstToDelete = if (player.name > opponent.name) player.name else opponent.name
-                        val secondToDelete = if (player.name > opponent.name) opponent.name else player.name
-                        deletePlayerInfo(firstToDelete, request.battleId, request.round).onComplete {
+                      case Success(opponentInfo) if opponentInfo != null =>
+                        TurnOrderingHelper.cleanRequestsAndSendOrderedTurns(requestInfo, opponentInfo)
 
-                          case Success(deleteResult1) if deleteResult1.getDeletedCount != 0 =>
-                            deletePlayerInfo(secondToDelete, request.battleId, request.round).onComplete {
-
-                              case Success(deleteResult2) if deleteResult2.getDeletedCount != 0 =>
-                                import Helper._
-                                incrementCurrentRound(request.battleId).onComplete {
-
-                                  case Success(_) =>
-                                    val allSpeedsOrdered = (extractTeamSpeeds(opponent) ++ extractTeamSpeeds(player))
-                                      .sortBy { case (_, speed: Int) => speed }
-                                      .reverse
-                                      .map { case (key: CharacterKey, _) => key }
-                                    val response = StartRoundResponse(Right(allSpeedsOrdered), request.round)
-
-                                    rabbitControl ! Message.queue(response, replyTo.get)
-                                    rabbitControl ! Message.queue(response, opponent.replyTo)
-
-                                  case Failure(e) => println(s"$LogFailurePrefix$e")
-
-                                }
-                              case Success(_) => println("Cannot delete what is not present...")
-                              case Failure(e) => println(s"$LogFailurePrefix$e")
-                            }
-                          case Success(_) => println("Cannot delete what is not present...")
-                          case Failure(e) => println(s"$LogFailurePrefix$e")
-                        }
                       case Success(_) => println("Opponent not found...")
                       case Failure(e) => println(s"$LogFailurePrefix$e")
                     }
@@ -97,11 +79,63 @@ object Main extends App {
     }
   }
 
-  private object Helper {
+  private object TurnOrderingHelper {
 
+    /** Removes both the saved client requests and informs them with the list of
+      * ordered turns concerning the appropriate turn.
+      *
+      * @param requestInfo the info about the player who made the second request
+      * @param opponentInfo the info about the player who made the first request
+      */
+    def cleanRequestsAndSendOrderedTurns(requestInfo: PlayerInfo, opponentInfo: PlayerInfo): Unit = {
+      val firstToDelete = if (requestInfo.name > opponentInfo.name) requestInfo.name else opponentInfo.name
+      val secondToDelete = if (requestInfo.name > opponentInfo.name) opponentInfo.name else requestInfo.name
+      deletePlayerInfo(firstToDelete, requestInfo.battleId, requestInfo.round).onComplete {
+
+        case Success(deleteResult1) if deleteResult1.getDeletedCount != 0 =>
+          deletePlayerInfo(secondToDelete, requestInfo.battleId, requestInfo.round).onComplete {
+
+            case Success(deleteResult2) if deleteResult2.getDeletedCount != 0 =>
+              incrementCurrentRound(requestInfo.battleId).onComplete {
+
+                case Success(_) =>
+                  val allSpeedsOrdered = (extractTeamSpeeds(opponentInfo) ++ extractTeamSpeeds(requestInfo))
+                    .sortBy { case (_, speed: Int) => speed }
+                    .reverse
+                    .map { case (key: CharacterKey, _) => key }
+                  send(allSpeedsOrdered, requestInfo, opponentInfo)
+
+                case Failure(e) => println(s"$LogFailurePrefix$e")
+              }
+            case Success(_) => println("Cannot delete what is not present...")
+            case Failure(e) => println(s"$LogFailurePrefix$e")
+          }
+        case Success(_) => println("Cannot delete what is not present...")
+        case Failure(e) => println(s"$LogFailurePrefix$e")
+      }
+    }
+
+    /** Sends a message to both the battling players.
+      *
+      * @param speeds the list of tuples (characterOwner, characterName) in decreasing
+      *               order according to their speeds
+      * @param requestInfo the info about the player who made the second request
+      * @param opponentInfo the info about the player who made the first request
+      */
+    def send(speeds: List[CharacterKey], requestInfo: PlayerInfo, opponentInfo: PlayerInfo): Unit = {
+      val response = StartRoundResponse(Right(speeds), requestInfo.round)
+
+      rabbitControl ! Message.queue(response, requestInfo.replyTo)
+      rabbitControl ! Message.queue(response, opponentInfo.replyTo)
+    }
+
+    /** Extracts a list of tuples (character, speed) from the player team.
+      *
+      * @param player structure that contains the player team
+      * @return list of tuples (character, speed)
+      */
     def extractTeamSpeeds(player: PlayerInfo): List[(CharacterKey, Int)] = {
       player.teamSpeeds.map { case (characterName, speed) => (player.name, characterName) -> speed }.toList
     }
   }
-
 }
