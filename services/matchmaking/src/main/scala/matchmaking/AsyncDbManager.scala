42346b85c6d543201f6e174194f4b26e48faecf0
Marco Canducci <marco.candoz@gmail.com>
2018-12-20 00:58:38 +0100

    Update documentation

--- services/matchmaking/src/main/scala/matchmaking/AsyncDbManager.scala
+++ services/matchmaking/src/main/scala/matchmaking/AsyncDbManager.scala
@@ -22,7 +22,8 @@ trait AsyncDbManager {
     * @param ticket the player's ticket for the matchmaking queue
     * @param playerInfo the player's information
     * @param replyTo the player response queue
-    * @return a Future representing the success/failure of the operation
+    * @return a Future representing the success/failure of the operation,
+    *         containing a string with a brief summary of the update
     */
   def putPlayerInQueue(ticket: Int, playerInfo: PlayerInfo, replyTo: String): Future[String]
 
@@ -39,21 +40,25 @@ trait AsyncDbManager {
   /** Removes the existing document of a queued player given its name.
     *
     * @param playerName the player to remove
-    * @return a Future representing the success/failure of the deletion
+    * @return a Future representing the success/failure of the operation,
+    *         containing true if the player is found and removed, false otherwise.
     */
   def removePlayerFromQueue(playerName: String): Future[Boolean]
 
   /** Notifies that a player has left the game while waiting for an opponent.
     *
     * @param playerName the name of the player that left the queue
-    * @return a Future representing the success/failure of the update
+    * @return a Future representing the success/failure of the operation,
+    *         containing true if the player info are found and updated,
+    *         false otherwise.
     */
   def notifyPlayerLeft(playerName: String): Future[Boolean]
 
   /** Creates a new battle instance inside the battles collection.
     *
     * @param battleId the battle identifier
-    * @return a Future representing the success/failure of the operation
+    * @return a Future representing the success/failure of the operation,
+    *         containing a string with a brief summary of the update
     */
   def createBattleInstance(battleId: String): Future[String]
 
